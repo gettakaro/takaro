634e0b7a5c858d1bbb79d84f638d82c3f5114e04
Niek Candaele <niek@candaele.dev>
2022-09-23 00:22:45 +0200

    revert mock emitter interval default

--- packages/lib-gameserver/src/gameservers/mock/emitter.ts
+++ packages/lib-gameserver/src/gameservers/mock/emitter.ts
@@ -14,7 +14,7 @@ const allEvents = Object.keys(GameEvents);
 
 class IMockConfig {
   allowedEvents = allEvents;
-  interval = 1000;
+  interval = 60000;
 
   constructor(config?: Partial<IMockConfig>) {
     if (!config) return;
