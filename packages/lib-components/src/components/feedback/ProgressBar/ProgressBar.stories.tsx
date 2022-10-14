2726b41aaf83bb4f995207834b7ad15685b406c3
Emiel Van Severen <35073890+emielvanseveren@users.noreply.github.com>
2022-10-14 23:59:28 +0200

    Style: reposition buttons

--- packages/lib-components/src/components/feedback/ProgressBar/ProgressBar.stories.tsx
+++ packages/lib-components/src/components/feedback/ProgressBar/ProgressBar.stories.tsx
@@ -1,24 +1,36 @@
 import { Meta, Story } from '@storybook/react';
 import { useState } from 'react';
 import { ProgressBar, ProgressBarProps } from '.';
+import { styled } from '../../../styled';
+
+const ButtonContainer = styled.div`
+  display: flex;
+  button {
+    &:first-child {
+      margin: 0 1rem;
+    }
+  }
+`;
 
 export default {
   title: 'Feedback/ProgressBar',
   component: ProgressBar
 } as Meta;
 
-export const Determinate: Story<ProgressBarProps> = (args) => {
+export const Determinate: Story<ProgressBarProps> = () => {
   const [value, setValue] = useState<number>(0);
 
   return (
     <>
       <ProgressBar mode="determinate" showValue value={value} />
-      <button onClick={() => setValue(value + 5)}>+5</button>
-      <button onClick={() => setValue(value + 10)}>+10</button>
+      <ButtonContainer>
+        <button onClick={() => setValue(value + 5)}>+5</button>
+        <button onClick={() => setValue(value + 10)}>+10</button>
+      </ButtonContainer>
     </>
   );
 };
 
-export const InDeterminate: Story<ProgressBarProps> = (args) => (
+export const InDeterminate: Story<ProgressBarProps> = () => (
   <ProgressBar mode="indeterminate" />
 );
