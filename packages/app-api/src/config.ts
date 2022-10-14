f620bd11039e3acb2870011abcc43cd3e39d2a1b
Niek Candaele <niek@candaele.dev>
2022-10-14 22:25:48 +0200

    fix anys in http port validation

--- packages/app-api/src/config.ts
+++ packages/app-api/src/config.ts
@@ -1,5 +1,6 @@
 import { Config, IBaseConfig } from '@takaro/config';
 import { queuesConfigSchema, IQueuesConfig } from '@takaro/queues';
+import { errors } from '@takaro/logger';
 
 enum CLUSTER_MODE {
   SINGLE = 'single',
@@ -27,16 +28,21 @@ const configSchema = {
       doc: 'The port to bind.',
       // This value can be ANYTHING because it is user provided
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      format: (value: any) => {
+      format: (value: unknown) => {
         if (process.env.NODE_ENV === 'test') {
           // This allows us to pass 'undefined' as the port
           // Which lets the tests run without needed to open an actual port
           return value;
         }
-        value = parseInt(value, 10);
 
-        if (value < 0 || value > 65535) {
-          throw new Error('ports must be within range 0 - 65535');
+        if (typeof value !== 'string' && typeof value !== 'number') {
+          throw new errors.ConfigError('Value must be a string or number');
+        }
+
+        const parsed = parseInt(value.toString(), 10);
+
+        if (parsed < 0 || parsed > 65535) {
+          throw new errors.ConfigError('ports must be within range 0 - 65535');
         }
       },
       default: 3000,
