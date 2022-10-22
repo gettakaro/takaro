d7a9d6559096e5a6dde2a9ddc6ae53032b8884ea
Emiel Van Severen <35073890+emielvanseveren@users.noreply.github.com>
2022-10-22 10:13:52 +0200

    Chore: remove old file explorer

--- packages/lib-components/src/components/navigation/index.ts
+++ packages/lib-components/src/components/navigation/index.ts
@@ -7,6 +7,3 @@ export type { TabSwitchProps } from './Tab';
 export { Stepper } from './Steppers/Stepper';
 export { SlimStepper } from './Steppers/SlimStepper';
 export { StepperProvider, useStepper } from './Steppers/context';
-
-export { Explorer } from './Explorer';
-export type { IExplorerProps } from './Explorer';
