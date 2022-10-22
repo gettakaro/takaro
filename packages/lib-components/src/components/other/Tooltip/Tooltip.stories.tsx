28192dccc6c591c2b1c94a8417fe3504c24942af
Emiel Van Severen <35073890+emielvanseveren@users.noreply.github.com>
2022-10-22 14:05:24 +0200

    Feat: <Tooltip/>

---
+++ packages/lib-components/src/components/other/Tooltip/Tooltip.stories.tsx
@@ -0,0 +1,41 @@
+import { FloatingDelayGroup } from '@floating-ui/react-dom-interactions';
+import { Meta, StoryFn } from '@storybook/react';
+import { Tooltip, TooltipProps } from '.';
+import { styled } from '../../../styled';
+
+const Container = styled.div`
+  display: flex;
+  justify-content: space-evenly 
+  `;
+
+export default {
+  title: 'Other/Tooltip',
+  component: Tooltip,
+  args: {
+    placement: 'bottom',
+    label: 'I am the label'
+  }
+} as Meta<TooltipProps>;
+
+export const Default: StoryFn<TooltipProps> = (args) => (
+   <Tooltip {...args}>
+     <a>I am content with a tooltip</a>
+   </Tooltip>
+  );
+
+export const Group: StoryFn<TooltipProps> = () => (
+  <Container>
+    If you go fast from one button to another you'll notice that the tooltip shows immediatly and ignores the initial delay
+ <FloatingDelayGroup delay={{ open: 1000, close: 200 }}>
+        <Tooltip label="Tooltip one">
+          <button>Hover me</button>
+        </Tooltip>
+        <Tooltip label="Tooltip two">
+          <button>Hover me</button>
+        </Tooltip>
+        <Tooltip label="Tooltip three">
+          <button>Hover me</button>
+        </Tooltip>
+      </FloatingDelayGroup>
+    </Container>
+)
