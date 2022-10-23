144fd7641e8e7838b47a37d8018d9d1a0b5a0113
Niek Candaele <niek@candaele.dev>
2022-10-23 20:34:18 +0200

    feat: add TakaroDTO

---
+++ packages/lib-http/src/util/TakaroDTO.ts
@@ -0,0 +1,40 @@
+import { errors, logger } from '@takaro/logger';
+import { Allow, validate } from 'class-validator';
+import { classToPlain } from 'class-transformer';
+import { Exclude } from 'class-transformer';
+
+/**
+ * Generic Data Transfer Object, used widely in Takaro to pass data back and forth between components
+ * Allows validation of properties when instantiated and JSON (de)serialization
+ */
+export class TakaroDTO<T> {
+  @Allow()
+  @Exclude()
+  log = logger('TakaroDTO');
+
+  constructor(data: Partial<T> = {}) {
+    Object.assign(this, data);
+  }
+
+  async validate() {
+    const validationErrors = await validate(this, {
+      forbidUnknownValues: true,
+      forbidNonWhitelisted: true,
+      whitelist: true,
+    });
+
+    if (validationErrors.length) {
+      const msg = `${validationErrors[0]}`;
+      this.log.warn(msg, validationErrors);
+      throw new errors.ValidationError(msg, validationErrors);
+    }
+  }
+
+  toJSON() {
+    return classToPlain(this, {});
+  }
+}
+
+export function isTakaroDTO<T>(value: unknown): value is TakaroDTO<T> {
+  return value instanceof TakaroDTO;
+}
