6141bff87da30f37f5540a5bd2c8770a20fae3fa
Emiel Van Severen <35073890+emielvanseveren@users.noreply.github.com>
2022-10-25 00:56:17 +0200

    Feat: <CollapseList/>

---
+++ packages/lib-components/src/components/other/CollapseList/CollapseList.stories.tsx
@@ -0,0 +1,17 @@
+import { Meta, StoryFn } from '@storybook/react';
+import { CollapseList } from '.';
+
+export default {
+  title: 'Navigation/CollapseList',
+  component: CollapseList,
+} as Meta;
+
+export const Default: StoryFn = (args) => {
+  return (
+    <CollapseList {...args}>
+      <CollapseList.Item title="Files">
+        <div> I am a container showcasing the files</div>
+      </CollapseList.Item>
+    </CollapseList>
+  );
+};
