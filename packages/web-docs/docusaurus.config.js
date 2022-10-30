96655a5e969caccf2a1769fb99a72ff453f5fcc1
Niek Candaele <niek@candaele.dev>
2022-10-30 21:21:07 +0100

    docs: remove the homepage, directly serve docs

--- packages/web-docs/docusaurus.config.js
+++ packages/web-docs/docusaurus.config.js
@@ -21,8 +21,8 @@ async function createConfig() {
 
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
-    organizationName: 'facebook', // Usually your GitHub org/user name.
-    projectName: 'docusaurus', // Usually your repo name.
+    organizationName: 'niekcandaele', // Usually your GitHub org/user name.
+    projectName: 'takaro', // Usually your repo name.
 
     // Even if you don't use internalization, you can use this field to set useful
     // metadata like html lang. For example, if your site is Chinese, you may want
@@ -38,6 +38,7 @@ async function createConfig() {
         /** @type {import('@docusaurus/preset-classic').Options} */
         ({
           docs: {
+            routeBasePath: '/',
             remarkPlugins: [],
             sidebarPath: require.resolve('./sidebars.js'),
             // Please change this to your repo.
@@ -45,13 +46,7 @@ async function createConfig() {
             editUrl:
               'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
           },
-          blog: {
-            showReadingTime: true,
-            // Please change this to your repo.
-            // Remove this to remove the "edit this page" links.
-            editUrl:
-              'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-          },
+          blog: false,
           theme: {
             customCss: require.resolve('./src/css/custom.css'),
           },
@@ -71,13 +66,18 @@ async function createConfig() {
           items: [
             {
               type: 'doc',
-              docId: 'development/getting-started',
+              docId: 'application/modules',
               position: 'left',
               label: 'Docs',
             },
-            { to: '/blog', label: 'Blog', position: 'left' },
             {
-              href: 'https://github.com/facebook/docusaurus',
+              type: 'doc',
+              docId: 'development/getting-started',
+              position: 'left',
+              label: 'Development',
+            },
+            {
+              href: 'https://github.com/niekcandaele/takaro',
               label: 'GitHub',
               position: 'right',
             },
@@ -99,16 +99,8 @@ async function createConfig() {
               title: 'Community',
               items: [
                 {
-                  label: 'Stack Overflow',
-                  href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-                },
-                {
                   label: 'Discord',
-                  href: 'https://discordapp.com/invite/docusaurus',
-                },
-                {
-                  label: 'Twitter',
-                  href: 'https://twitter.com/docusaurus',
+                  href: 'https://discord.catalysm.net',
                 },
               ],
             },
@@ -116,17 +108,13 @@ async function createConfig() {
               title: 'More',
               items: [
                 {
-                  label: 'Blog',
-                  to: '/blog',
-                },
-                {
                   label: 'GitHub',
                   href: 'https://github.com/niekcandaele/takaro',
                 },
               ],
             },
           ],
-          copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+          copyright: `Copyright © ${new Date().getFullYear()} Takaro.`,
         },
         prism: {
           theme: lightCodeTheme,
