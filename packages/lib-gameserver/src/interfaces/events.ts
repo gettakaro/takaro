ffe8666c8b72d10105c49af325a56079911ccf48
Niek Candaele <niek@candaele.dev>
2022-11-05 20:13:04 +0100

    fix: constructor of BaseEvent, this allows for better type suggestions when constructing event objects

--- packages/lib-gameserver/src/interfaces/events.ts
+++ packages/lib-gameserver/src/interfaces/events.ts
@@ -7,15 +7,17 @@ export enum GameEvents {
   LOG_LINE = 'log',
   PLAYER_CONNECTED = 'player-connected',
   PLAYER_DISCONNECTED = 'player-disconnected',
+  CHAT_MESSAGE = 'chat-message',
 }
 
 export type EventMapping = {
   [GameEvents.LOG_LINE]: EventLogLine;
   [GameEvents.PLAYER_CONNECTED]: EventPlayerConnected;
   [GameEvents.PLAYER_DISCONNECTED]: EventPlayerDisconnected;
+  [GameEvents.CHAT_MESSAGE]: EventChatMessage;
 };
 
-export class BaseEvent extends TakaroDTO<BaseEvent> {
+export class BaseEvent<T> extends TakaroDTO<BaseEvent<T>> {
   @IsISO8601()
   timestamp: string;
 
@@ -25,25 +27,34 @@ export class BaseEvent extends TakaroDTO<BaseEvent> {
   @IsString()
   msg: string;
 
-  constructor(data: Record<string, unknown>) {
+  constructor(data: Partial<T>) {
     super({ ...data, timestamp: new Date().toISOString() });
   }
 }
 
-export class EventLogLine extends BaseEvent {
+export class EventLogLine extends BaseEvent<EventLogLine> {
   type = GameEvents.LOG_LINE;
 }
 
-export class EventPlayerConnected extends BaseEvent {
+export class EventPlayerConnected extends BaseEvent<EventPlayerConnected> {
   type = GameEvents.PLAYER_CONNECTED;
   @ValidateNested()
   @Type(() => IGamePlayer)
   player: IGamePlayer;
 }
 
-export class EventPlayerDisconnected extends BaseEvent {
+export class EventPlayerDisconnected extends BaseEvent<EventPlayerDisconnected> {
   type = GameEvents.PLAYER_DISCONNECTED;
   @ValidateNested()
   @Type(() => IGamePlayer)
   player: IGamePlayer;
+}
+
+export class EventChatMessage extends BaseEvent<EventChatMessage> {
+  type = GameEvents.CHAT_MESSAGE;
+  @ValidateNested()
+  @Type(() => IGamePlayer)
+  player?: IGamePlayer;
+
+  msg: string;
 }
