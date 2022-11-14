aa556bf7a82c87d47276fae5f1cff5d62925b9cc
Emiel Van Severen <35073890+emielvanseveren@users.noreply.github.com>
2022-11-14 12:07:42 +0100

    Fix: remove redundant input fields

--- packages/lib-components/src/components/inputs/Checkbox/index.tsx
+++ packages/lib-components/src/components/inputs/Checkbox/index.tsx
@@ -6,7 +6,6 @@ import {
   CheckMarkContainer,
   Container,
   Label,
-  Input
 } from './style';
 import { AiOutlineCheck as Icon } from 'react-icons/ai';
 import { Control, useController } from 'react-hook-form';
@@ -20,12 +19,11 @@ export interface CheckboxProps {
   readOnly?: boolean;
   label?: string;
   labelPosition?: 'left' | 'right';
-  onChange?: (e: React.MouseEvent<HTMLDivElement | HTMLLabelElement>) => void;
 }
 
 const variants = {
   checked: { scale: 1 },
-  unchecked: { scale: 0, opacity: 0 }
+  unchecked: { scale: 0, opacity: 0 },
 };
 
 export const Checkbox: FC<CheckboxProps> = ({
@@ -36,27 +34,19 @@ export const Checkbox: FC<CheckboxProps> = ({
   labelPosition = 'right',
   name,
   loading = false,
-  onChange
 }) => {
   const [isChecked, setChecked] = useState<boolean>(defaultValue);
   const { field: checkbox } = useController({
     name,
     control,
-    defaultValue: defaultValue
+    defaultValue: defaultValue,
   });
 
-  function onCheck(e: React.MouseEvent<HTMLDivElement | HTMLLabelElement>): void {
+  function onCheck(): void {
     if (readOnly) {
       return;
     }
     setChecked(!isChecked);
-
-    // check if parent function is defined
-    if (typeof onChange === 'function') {
-      setTimeout(() => {
-        onChange(e);
-      }, 100); // timeout because the value change takes a second
-    }
   }
 
   useEffect(() => {
@@ -72,7 +62,11 @@ export const Checkbox: FC<CheckboxProps> = ({
             {label}
           </Label>
         )}
-        <CheckboxContainer className="placeholder" isChecked={isChecked} readOnly={readOnly} />
+        <CheckboxContainer
+          className="placeholder"
+          isChecked={isChecked}
+          readOnly={readOnly}
+        />
         {/* CASE: show label after <CheckBox /> */}
         {labelPosition === 'right' && label && (
           <Label onClick={onCheck} position={labelPosition}>
@@ -91,7 +85,11 @@ export const Checkbox: FC<CheckboxProps> = ({
           {label}
         </Label>
       )}
-      <CheckboxContainer isChecked={isChecked} onClick={onCheck} readOnly={readOnly}>
+      <CheckboxContainer
+        isChecked={isChecked}
+        onClick={onCheck}
+        readOnly={readOnly}
+      >
         <BackgroundContainer
           animate={isChecked ? 'checked' : 'unchecked'}
           transition={getTransition()}
@@ -100,21 +98,6 @@ export const Checkbox: FC<CheckboxProps> = ({
         <CheckMarkContainer isChecked={isChecked}>
           <Icon size={18} />
         </CheckMarkContainer>
-
-        {/*
-            ##########################################
-            Ignore this input field it is just here
-            for the controller, but it is not visible for the user.
-            ##########################################
-          */}
-        <Input
-          {...checkbox}
-          checked={isChecked}
-          id={name}
-          name={name}
-          readOnly={readOnly}
-          type="checkbox"
-        />
       </CheckboxContainer>
       {/* CASE: show label after <CheckBox /> */}
       {labelPosition === 'right' && label && (
--- packages/lib-components/src/components/inputs/Switch/index.tsx
+++ packages/lib-components/src/components/inputs/Switch/index.tsx
@@ -1,14 +1,12 @@
-import { FC, useState } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { getTransition } from '../../../helpers';
 import { Container, Dot, Line, Label } from './style';
 import { useController, Control } from 'react-hook-form';
 export interface SwitchProps {
-  /* Unique name, required to toggle the switch */
   name: string;
   control: Control<any>;
   defaultValue?: boolean;
   disabled?: boolean;
-  onChange?: (isChecked: boolean) => void;
 }
 
 export const Switch: FC<SwitchProps> = ({
@@ -16,28 +14,20 @@ export const Switch: FC<SwitchProps> = ({
   control,
   defaultValue = false,
   disabled = false,
-  onChange
 }) => {
   const [isChecked, setChecked] = useState(defaultValue);
-  const { field: sw } = useController({ name, control });
+  const { field } = useController({ name, control });
 
-  function onCheck(): void {
+  function handleOnClick(): void {
     setChecked(!isChecked);
-    if (typeof onChange === 'function') onChange(sw.value);
   }
 
+  useEffect(() => {
+    field.onChange(isChecked);
+  }, [isChecked]);
+
   return (
-    <Container>
-      {/* this is the input component itself, but cannot be styled properly. */}
-      <input
-        disabled={disabled}
-        id={name}
-        name={name}
-        onChange={onCheck}
-        ref={sw.ref}
-        style={{ display: 'none' }}
-        type="checkbox"
-      ></input>
+    <Container onClick={handleOnClick}>
       <Label htmlFor={name}>
         <Line disabled={disabled} isChecked={isChecked}>
           <Dot
