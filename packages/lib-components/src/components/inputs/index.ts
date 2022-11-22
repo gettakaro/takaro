9cbf0ff0b0deb5ec9911a055dac40905f6641b13
Emiel Van Severen <35073890+emielvanseveren@users.noreply.github.com>
2022-11-22 10:24:31 +0100

    Move buttons to actions dir

--- packages/lib-components/src/components/inputs/index.ts
+++ packages/lib-components/src/components/inputs/index.ts
@@ -1,17 +1,8 @@
-export { Button } from './Button';
-export type { ButtonProps } from './Button';
-
 export { Checkbox } from './Checkbox';
 export type { CheckboxProps } from './Checkbox';
 
 export { Select, OptionGroup, Option } from './Select';
 export type { SelectProps, OptionProps, OptionGroupProps } from './Select';
-
-export { ToggleButtonGroup, ToggleButton } from './ToggleButton';
-export type { ToggleButtonGroupProps, ToggleButtonProps } from './ToggleButton';
-
-export { IconButton } from './IconButton';
-export type { IconButtonProps } from './IconButton';
 
 export { Switch } from './Switch';
 export type { SwitchProps } from './Switch';
@@ -20,9 +11,6 @@ export { TextField } from './TextField';
 export type { FieldProps } from './Field';
 
 export { SearchField } from './SearchField';
-
-export { SupportButton } from './SupportButton';
-export type { SupportButtonProps } from './SupportButton';
 
 export { SliderComponent as Slider } from './Slider';
 export type { SliderProps } from './Slider';
