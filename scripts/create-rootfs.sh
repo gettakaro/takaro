9ea5800f8a5f19e911645dce1eb4c9ed85be525a
Branco <branco.bruyneel@gmail.com>
2022-12-03 17:24:05 +0100

    chore: fix rootfs script

--- scripts/create-rootfs.sh
+++ scripts/create-rootfs.sh
@@ -2,18 +2,18 @@
 
 set -e
 
-source ../.env
+source .env
 
 # create an empty rootfs
-dd if=/dev/zero of=rootfs.ext4 bs=1M count=100
+dd if=/dev/zero of="$FIRECRACKER_ROOTFS" bs=1M count=100
 mkfs.ext4 "$FIRECRACKER_ROOTFS"
-mkdir -p /tmp/takaro/
-mount "$FIRECRACKER_ROOTFS" /tmp/takaro/my-rootfs
+mkdir -p /tmp/takaro/my-rootfs
+mount "$FIRECRACKER_ROOTFS" /tmp/takaro/my-rootfs/
 
 docker run -i --rm \
 	-v /tmp/takaro/my-rootfs:/my-rootfs \
-	-v "../packages/vm-agent/target/release/vm-agent:/usr/local/bin/agent" \
-	-v "../packages/vm-agent/agent-service:/etc/init.d/agent" \
-	alpine sh <./setup-image.sh
+	-v "$VM_AGENT_BINARY:/usr/local/bin/agent" \
+	-v "$VM_AGENT_SERVICE:/etc/init.d/agent" \
+	alpine sh <./scripts/setup-image.sh
 
-umount /tmp/takaro/my-rootfs
+umount /tmp/takaro/my-rootfs/
