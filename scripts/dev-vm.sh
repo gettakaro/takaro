c5460d695b24727fcd2f39d66f9f9edd4e5141f5
Emiel Van Severen <35073890+emielvanseveren@users.noreply.github.com>
2023-02-25 20:18:51 +0100

    Fix: build vm agent from dev-vm

--- scripts/dev-vm.sh
+++ scripts/dev-vm.sh
@@ -1,28 +1,17 @@
 #!/bin/bash
 # NOTE: this script should run from the repo root.
 
-# Reset sockets if they exist
-mkdir -p /tmp/takaro
-rm /tmp/takaro/*.socket
-
-# TODO: use cargo build from outside the vm-agent directory
-function build_agent() {
-	cd ./packages/vm-agent
-	./build.sh
-	cd ../..
-}
-build_agent
-
 FIRECRACKER_ROOTFS=./packages/app-agent/src/lib/firecracker/rootfs.ext4
+VM_AGENT_BINARY=./packages/vm-agent/target/release/vm-agent
+VM_AGENT_SERVICE=./packages/vm-agent/agent-service
+
+cargo build --manifest-path=./packages/vm-agent/Cargo.toml
 
 # create an empty rootfs
 dd if=/dev/zero of="$FIRECRACKER_ROOTFS" bs=1M count=150
 mkfs.ext4 "$FIRECRACKER_ROOTFS"
 mkdir -p /tmp/takaro/my-rootfs
 sudo mount "$FIRECRACKER_ROOTFS" /tmp/takaro/my-rootfs/
-
-VM_AGENT_BINARY=./packages/vm-agent/target/release/vm-agent
-VM_AGENT_SERVICE=./packages/vm-agent/agent-service
 
 docker run -i --rm \
 	-v /tmp/takaro/my-rootfs:/my-rootfs \
@@ -31,6 +20,10 @@ docker run -i --rm \
 	alpine sh <./containers/vm-agent-image/build.sh
 
 sudo umount /tmp/takaro/my-rootfs/
+
+# Reset sockets if they exist
+mkdir -p /tmp/takaro
+rm /tmp/takaro/*.socket
 
 echo "Starting firecracker..."
 
