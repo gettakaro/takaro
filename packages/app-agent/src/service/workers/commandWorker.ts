432fe27f5980513fa122d0aa5b6f312c5a2ff641
Niek Candaele <niek@candaele.dev>
2023-02-25 15:05:28 +0100

    feat: add workers for commands and hooks

---
+++ packages/app-agent/src/service/workers/commandWorker.ts
@@ -0,0 +1,22 @@
+import { Job } from 'bullmq';
+import { Client } from '@takaro/apiclient';
+import { config } from '../../config';
+import { TakaroWorker, IJobData } from '@takaro/queues';
+import { executeFunction } from './executeFunction';
+
+export class CommandWorker extends TakaroWorker<IJobData> {
+  constructor() {
+    super(config.get('queues.commands.name'), processCommand);
+  }
+}
+
+async function processCommand(job: Job<IJobData>) {
+  const client = new Client({
+    auth: {
+      token: job.data.token,
+    },
+    url: config.get('takaro.url'),
+  });
+
+  await executeFunction(job.data.function, client);
+}
