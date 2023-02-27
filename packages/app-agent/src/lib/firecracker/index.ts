6aa1fe7dfb7d964ebbfb18dfa9e09df2fec1e57d
Branco <branco.bruyneel@gmail.com>
2023-02-27 21:53:27 +0100

    chore: comment out network device

--- packages/app-agent/src/lib/firecracker/index.ts
+++ packages/app-agent/src/lib/firecracker/index.ts
@@ -137,13 +137,13 @@ export default class Firecracker {
 
       this.logger.debug('setting up network');
 
-      await this.httpSock.put('network-interfaces/enp0s25', {
-        json: {
-          iface_id: 'enp0s25',
-          guest_mac: 'AA:FC:00:00:00:01',
-          host_dev_name: 'tap0',
-        },
-      });
+      // await this.httpSock.put('network-interfaces/enp0s25', {
+      //   json: {
+      //     iface_id: 'enp0s25',
+      //     guest_mac: 'AA:FC:00:00:00:01',
+      //     host_dev_name: 'tap0',
+      //   },
+      // });
     } catch (err) {
       this.logger.error('setting up vm failed', err);
     }
