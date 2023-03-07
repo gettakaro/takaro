1d7a60ace1e006ff9936b389807aea802805baff
Niek Candaele <22315101+niekcandaele@users.noreply.github.com>
2023-03-07 22:39:30 +0100

    feat(ci): add Docker build for web-main (#100)

---
+++ containers/generic-web/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+FILE_LOCATION=/usr/share/nginx/html/env.js
+
+# Recreate config file
+rm -rf "$FILE_LOCATION"
+touch "$FILE_LOCATION"
+
+# Add assignment
+echo "window.__env__ = {" >>"$FILE_LOCATION"
+echo "  REACT_APP_API: \"$REACT_APP_API\"," >>"$FILE_LOCATION"
+echo "}" >>"$FILE_LOCATION"
+
+nginx -g "daemon off;"
--- packages/web-main/src/hooks/useApiClient.ts
+++ packages/web-main/src/hooks/useApiClient.ts
@@ -2,15 +2,23 @@ import { Client } from '@takaro/apiclient';
 
 let cachedClient: Client | null = null;
 
+declare global {
+  interface Window {
+    __env__: Record<string, string>;
+  }
+}
+
 export const useApiClient = () => {
   if (cachedClient) {
     return cachedClient;
   }
 
-  if (!process.env.REACT_APP_API) throw new Error('REACT_APP_API is not set');
+  const apiUrl = window.__env__.REACT_APP_API || process.env.REACT_APP_API;
+
+  if (!apiUrl) throw new Error('REACT_APP_API is not set');
 
   cachedClient = new Client({
-    url: process.env.REACT_APP_API,
+    url: apiUrl,
     auth: {},
     log: {
       debug: () => {},
