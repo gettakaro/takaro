aaf587e556f984c7ee0eda2b6af3a0778d4e13dd
Niek Candaele <niek@candaele.dev>
2023-03-13 01:23:45 +0100

    fix: remove domain as label for metrics middleware
    it'd create too many time series, let's keep it limited for now

--- packages/lib-http/src/middleware/metrics.ts
+++ packages/lib-http/src/middleware/metrics.ts
@@ -4,13 +4,13 @@ import { Counter, Histogram } from 'prom-client';
 const counter = new Counter({
   name: 'http_requests_total',
   help: 'Total number of HTTP requests made',
-  labelNames: ['path', 'method', 'status', 'domain'],
+  labelNames: ['path', 'method', 'status'],
 });
 
 const histogram = new Histogram({
   name: 'http_request_duration_seconds',
   help: 'Duration of HTTP requests in seconds',
-  labelNames: ['path', 'method', 'status', 'domain'],
+  labelNames: ['path', 'method', 'status'],
   buckets: [0.1, 0.5, 1, 2, 5, 10],
 });
 
