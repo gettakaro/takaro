83b0b98b241bab25a6771cf1f8245e8a2942ad7b
Branco <branco.bruyneel@gmail.com>
2023-03-19 13:47:32 +0100

    fix: logger init

--- packages/lib-util/src/sleep.ts
+++ packages/lib-util/src/sleep.ts
@@ -1,8 +1,8 @@
 import { logger } from './logger.js';
 
-const log = logger('sleep');
-
 export async function sleep(ms: number) {
+  const log = logger('sleep');
+
   log.debug(`sleeping for ${ms}ms`);
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
