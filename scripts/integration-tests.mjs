7aa88d36ef3e95312ee98af7bb7da16161358ac9
Niek Candaele <niek@candaele.dev>
2023-03-19 16:09:52 +0100

    chore: let integration tests create admin client if required

--- scripts/integration-tests.mjs
+++ scripts/integration-tests.mjs
@@ -1,13 +1,34 @@
 import { upOne, upMany, logs, exec } from 'docker-compose';
+import { OAuth2Api, Configuration } from '@ory/client';
 
 const composeOpts = { log: true, composeOptions: ['-f', 'docker-compose.test.yml'], env: { ...process.env } };
 
 async function main() {
   // First, start the datastores
-  await upMany(['postgresql', 'redis'], composeOpts);
+  await upMany(['postgresql', 'redis', 'postgresql_kratos', 'postgresql_hydra'], composeOpts);
 
+  // Then, start supporting services
+  await upMany(['kratos', 'hydra'], composeOpts);
 
-  // Once all data stores are initialized, we can start the app itself
+  // Check if ADMIN_CLIENT_ID and ADMIN_CLIENT_SECRET are set already
+  // If not set, create them
+  if (!composeOpts.env.ADMIN_CLIENT_ID || !composeOpts.env.ADMIN_CLIENT_SECRET) {
+    const hydraAdmin = new OAuth2Api(
+      new Configuration({
+        basePath: 'http://localhost:4445',
+      }));
+
+    const { data: { client_id, client_secret } } = await hydraAdmin.createOAuth2Client({
+      oAuth2Client: {
+        grant_types: ['client_credentials'],
+      }
+    });
+
+    console.log('Created OAuth admin client', { client_id });
+    composeOpts.env.ADMIN_CLIENT_ID = client_id;
+    composeOpts.env.ADMIN_CLIENT_SECRET = client_secret;
+  }
+
   await upOne('takaro', composeOpts);
 
   let failed = false;
