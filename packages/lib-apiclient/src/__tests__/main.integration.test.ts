e97bc6c307b66770a172dad1f4aadb1cb7e04853
Niek Candaele <niek@candaele.dev>
2023-03-19 17:44:25 +0100

    fix: a test in apiclient

--- packages/lib-apiclient/src/__tests__/main.integration.test.ts
+++ packages/lib-apiclient/src/__tests__/main.integration.test.ts
@@ -1,11 +1,9 @@
-import { AdminClient, Client } from '../main.js';
+import { AdminClient } from '../main.js';
 import { expect, integrationConfig } from '@takaro/test';
 import { AxiosResponse } from 'axios';
 import { DomainCreateOutputDTOAPI } from '../generated/index.js';
 
 const TEST_DOMAIN_NAME = 'apiClient-test';
-
-const TEST_TAKARO_DEV_USER_NAME = 'apiClient-test-user';
 
 describe('API client', () => {
   const adminClient = new AdminClient({
@@ -59,36 +57,6 @@ describe('API client', () => {
           domain.data.data.createdDomain.id
         );
       }
-    });
-
-    it('Can create and delete a user', async () => {
-      if (!domain) throw new Error('Domain not created');
-
-      const client = new Client({
-        url: integrationConfig.get('host'),
-        auth: {
-          username: domain.data.data.rootUser.email,
-          password: domain.data.data.password,
-        },
-      });
-
-      await client.login();
-
-      const user = await client.user.userControllerCreate({
-        name: TEST_TAKARO_DEV_USER_NAME,
-        password: '123456789',
-        email: `${TEST_TAKARO_DEV_USER_NAME}@test.takaro.io`,
-      });
-
-      expect(user.data.data.name).to.equal(TEST_TAKARO_DEV_USER_NAME);
-
-      const usersRes = await client.user.userControllerSearch({
-        filters: { name: TEST_TAKARO_DEV_USER_NAME },
-      });
-
-      expect(usersRes.data.data).to.have.lengthOf(1);
-
-      await client.user.userControllerRemove(user.data.data.id);
     });
   });
 });
