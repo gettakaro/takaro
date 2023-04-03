9a0fa05bf18321aa8b89a14c9b7dcbc54a2184c0
Branco Bruyneel <brancobruyneel@flaptop.local>
2023-04-03 22:20:48 +0200

    docs: init nextra

---
+++ packages/web-docs/next.config.mjs
@@ -0,0 +1,13 @@
+import nextra from 'nextra';
+import { remarkMermaid } from 'remark-mermaid-nextra';
+
+const withNextra = nextra({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.tsx',
+  defaultShowCopyCode: true,
+  mdxOptions: {
+    remarkPlugins: [remarkMermaid],
+  },
+});
+
+export default withNextra({});
---
+++ packages/web-docs/theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { DocsThemeConfig } from 'nextra-theme-docs';
+
+const config: DocsThemeConfig = {
+  logo: <span>Takaro</span>,
+  project: {
+    link: 'https://github.com/gettakaro/takaro',
+  },
+  chat: {
+    link: 'https://aka.candaele.dev/discord',
+  },
+  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  footer: {
+    text: 'Takaro Documentation',
+  },
+};
+
+export default config;
