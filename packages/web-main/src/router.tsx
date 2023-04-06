3221372465642eb499f2c514710f40d6add76fe8
Emiel Van Severen <35073890+emielvanseveren@users.noreply.github.com>
2023-04-06 15:02:17 +0200

    Feat: use new React Router 6.4 data api

---
+++ packages/web-main/src/router.tsx
@@ -0,0 +1,101 @@
+import { lazy } from 'react';
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from 'react-router-dom';
+
+import Dashboard from 'pages/Dashboard';
+import { PATHS } from 'paths';
+import { AuthenticatedRoute } from 'components/AuthenticatedRoute';
+
+import GameServers from 'pages/GameServers';
+import Players from 'pages/Players';
+import { ModuleDefinitions } from 'pages/ModuleDefinitions';
+import { wrapCreateBrowserRouter } from '@sentry/react';
+import CreateUpdateGameServer from 'pages/CreateUpdateGameServer';
+
+import { SettingsFrame } from './frames/SettingsFrame';
+import { GlobalGameServerSettings } from './pages/settings/GlobalGameServerSettings';
+
+const sentryCreateBrowserRouter = wrapCreateBrowserRouter(createBrowserRouter);
+
+// Lazy load pages
+const LogIn = lazy(() => import('./pages/LogIn'));
+const Studio = lazy(() => import('./pages/studio'));
+const GameServerDashboard = lazy(
+  () => import('./pages/gameserver/GameServerDashboard')
+);
+const GameServerSettings = lazy(
+  () => import('./pages/gameserver/GameServerSettings')
+);
+const GameServerModules = lazy(
+  () => import('./pages/gameserver/GameServerModules')
+);
+const NotFound = lazy(() => import('./pages/NotFound'));
+
+export const router = sentryCreateBrowserRouter(
+  createRoutesFromElements(
+    <>
+      {/* ======================== Global ======================== */}
+      <Route
+        element={<AuthenticatedRoute frame="global" />}
+        path={PATHS.home()}
+      >
+        <Route element={<Dashboard />} path={PATHS.home()} />
+        <Route element={<SettingsFrame />}>
+          <Route
+            element={<GlobalGameServerSettings />}
+            path={PATHS.settings.GameServerSettings}
+          />
+        </Route>
+        <Route element={<GameServers />} path="/server/" />
+
+        <Route element={<GameServers />} path={PATHS.gameServers.overview()}>
+          <Route
+            element={<CreateUpdateGameServer />}
+            path={PATHS.gameServers.create()}
+          />
+          <Route
+            element={<CreateUpdateGameServer />}
+            path={PATHS.gameServers.update(':serverId')}
+          />
+        </Route>
+        <Route element={<Players />} path={PATHS.players()} />
+        <Route
+          element={<ModuleDefinitions />}
+          path={PATHS.moduleDefinitions()}
+        />
+      </Route>
+
+      {/* TODO: fix path, frame should be aware of /servers/serverId */}
+      {/* ======================== Game Server ======================== */}
+      <Route
+        element={<AuthenticatedRoute frame="gameserver" />}
+        path="/server/:serverId"
+      >
+        <Route
+          element={<GameServerDashboard />}
+          path={PATHS.gameServer.dashboard(':serverId')}
+        />
+        <Route
+          element={<GameServerSettings />}
+          path={PATHS.gameServer.settings(':serverId')}
+        />
+        <Route
+          element={<GameServerModules />}
+          path={PATHS.gameServer.modules(':serverId')}
+        />
+      </Route>
+
+      {/* ======================== Studio ======================== */}
+      <Route element={<AuthenticatedRoute frame="studio" />}>
+        <Route element={<Studio />} path={PATHS.studio.module(':moduleId')} />
+      </Route>
+      <Route element={<LogIn />} path={PATHS.login()} />
+
+      {/* Page not found matches with everything => should stay at bottom */}
+      <Route element={<NotFound />} path="*" />
+    </>
+  )
+);
