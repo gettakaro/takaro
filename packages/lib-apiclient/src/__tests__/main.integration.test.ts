8f2d6eae412388646e43366222087aba8e01f171
Niek Candaele <niek@candaele.dev>
2023-04-09 08:58:12 +0200

    fix: re-add a test that was deleted for debug reasons

---
+++ packages/lib-apiclient/src/__tests__/main.integration.test.ts
@@ -0,0 +1,42 @@
+import { AdminClient } from '../main.js';
+import { expect, integrationConfig } from '@takaro/test';
+
+const TEST_DOMAIN_NAME = 'apiClient-test';
+
+describe('API client', () => {
+  const adminClient = new AdminClient({
+    url: integrationConfig.get('host'),
+    auth: {
+      clientId: integrationConfig.get('auth.adminClientId'),
+      clientSecret: integrationConfig.get('auth.adminClientSecret'),
+    },
+    OAuth2URL: integrationConfig.get('auth.OAuth2URL'),
+  });
+
+  describe('Meta controller', () => {
+    it('should get health', async () => {
+      const health = await adminClient.meta.metaGetHealth();
+      expect(health.data.healthy).to.be.true;
+    });
+  });
+
+  describe('Domain controller', () => {
+    it('Can create and delete a domain', async () => {
+      const domain = await adminClient.domain.domainControllerCreate({
+        name: TEST_DOMAIN_NAME,
+      });
+      expect(domain.data.data.createdDomain.name).to.equal(TEST_DOMAIN_NAME);
+
+      const domainsRes = await adminClient.domain.domainControllerSearch({
+        filters: { name: TEST_DOMAIN_NAME },
+      });
+
+      expect(domainsRes.data.data).to.have.lengthOf(1);
+      expect(domainsRes.data.data[0].name).to.equal(TEST_DOMAIN_NAME);
+
+      await adminClient.domain.domainControllerRemove(
+        domain.data.data.createdDomain.id
+      );
+    });
+  });
+});
