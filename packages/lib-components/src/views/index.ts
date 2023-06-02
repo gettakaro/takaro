184c4328baab9dd955a347dc90d6d5503d954927
Branco <branco.bruyneel@gmail.com>
2023-06-02 11:29:08 +0200

    chore: export onboarding view

--- packages/lib-components/src/views/index.ts
+++ packages/lib-components/src/views/index.ts
@@ -3,3 +3,4 @@ export type { GetStartedProps } from './GetStarted';
 
 export { LoadingPage } from './LoadingPage';
 export { HeaderNav } from './HeaderNav';
+export { ModuleOnboarding } from './ModuleOnboarding';
