a5157507a47df526d1ee104d52ba2a670d68e942
Niek Candaele <niek@candaele.dev>
2023-06-04 15:59:07 +0200

    feat: add item configs to Studio for hooks, commands and cronjobs

---
+++ packages/web-main/src/components/modules/Editor/configs/commandConfig.tsx
@@ -0,0 +1,63 @@
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Button, TextAreaField, TextField } from '@takaro/lib-components';
+import { ModuleItemProperties } from 'context/moduleContext';
+import { useCommand, useCommandUpdate } from 'queries/modules';
+import { FC, useEffect } from 'react';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { z } from 'zod';
+
+interface IProps {
+  moduleItem: ModuleItemProperties;
+}
+
+interface IFormInputs {
+  trigger: string;
+  helpText: string;
+}
+
+const validationSchema = z.object({
+  trigger: z.string(),
+  helpText: z.string(),
+});
+
+export const CommandConfig: FC<IProps> = ({ moduleItem }) => {
+  const { data } = useCommand(moduleItem.itemId);
+  const { mutateAsync } = useCommandUpdate();
+
+  const { control, setValue, handleSubmit } = useForm<IFormInputs>({
+    mode: 'onSubmit',
+    resolver: zodResolver(validationSchema),
+  });
+
+  useEffect(() => {
+    if (data) {
+      setValue('trigger', data?.trigger);
+      setValue('helpText', data?.helpText);
+    }
+  }, [data]);
+
+  const onSubmit: SubmitHandler<IFormInputs> = async (data) => {
+    await mutateAsync({
+      commandId: moduleItem.itemId,
+      command: data,
+    });
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <TextField
+        control={control}
+        name="trigger"
+        label="trigger"
+        description="What users type ingame to trigger this command"
+      />
+      <TextAreaField
+        control={control}
+        name="helpText"
+        label="Help text"
+        description="What the command does"
+      />
+      <Button fullWidth type="submit" text="Save" />
+    </form>
+  );
+};
