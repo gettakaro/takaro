a4bcc7a45333774a41fafe8382ea288c1d72f9d2
Niek Candaele <niek@candaele.dev>
2023-06-10 15:41:12 +0200

    feat: basic rust connections working

--- packages/lib-gameserver/src/gameservers/rust/emitter.ts
+++ packages/lib-gameserver/src/gameservers/rust/emitter.ts
@@ -1,10 +1,9 @@
 import WebSocket from 'ws';
 import { logger, errors } from '@takaro/util';
-import { IsString } from 'class-validator';
-import { JsonObject } from 'type-fest';
 import { RustConnectionInfo } from './connectionInfo.js';
 import { TakaroEmitter } from '../../TakaroEmitter.js';
 import {
+  EventChatMessage,
   EventLogLine,
   EventPlayerConnected,
   EventPlayerDisconnected,
@@ -12,39 +11,23 @@ import {
 } from '../../interfaces/events.js';
 import { IGamePlayer } from '../../interfaces/GamePlayer.js';
 
-export class RustConfig {
-  @IsString()
-  hostname!: string;
-  @IsString()
-  port!: string;
-  @IsString()
-  password!: string;
-
-  constructor(config: JsonObject) {
-    Object.assign(this, config);
-  }
-}
-
 export enum RustEventType {
-  DEFAULT = 'generic',
-  WARNING = 'warning',
+  DEFAULT = 'Generic',
+  WARNING = 'Warning',
+  CHAT = 'Chat',
   // pretty sure chat messages have their own type as well.
 }
 
 const EventRegexMap = {
   [GameEvents.PLAYER_CONNECTED]: /\d{17}\/.+ joined \[.+\/\d{17}\]/,
   [GameEvents.PLAYER_DISCONNECTED]: /.* disconnecting\: disconnect/,
-  // [GameEvents.PLAYER_SPAWNED]: /.*\[\d{17}\] has spawned/,
-  // [GameEvents.PLAYER_KICKED]: /.*\/\d{17}\/.* kicked: .*/,
-  // [GameEvents.ITEM_GIVEN_TO]: /\[.*\] giving .*/,
-  // [GameEvents.PLAYER_MESSAGED]: /\[CHAT\].*\[\d{17}\] :.*/,
 };
 
 export interface RustEvent {
-  message: string;
-  identifier: number;
-  type: RustEventType;
-  stacktrace: string;
+  Message: string;
+  Identifier: number;
+  Type: RustEventType;
+  Stacktrace: string;
 }
 
 export class RustEmitter extends TakaroEmitter {
@@ -55,18 +38,22 @@ export class RustEmitter extends TakaroEmitter {
     super();
   }
 
+  static constructWs(config: RustConnectionInfo) {
+    const protocol = config.useTls ? 'wss' : 'ws';
+    return new WebSocket(
+      `${protocol}://${config.host}:${config.rconPort}/${config.rconPassword}`
+    );
+  }
+
   async start(): Promise<void> {
     return new Promise((resolve, reject) => {
       this.logger.debug('Connecting to [RUST] game server...');
 
-      this.ws = new WebSocket(
-        `ws://${this.config.host}:${this.config.rconPort}/${this.config.rconPassword}`
-      );
+      this.ws = RustEmitter.constructWs(this.config);
+      this.logger.debug('Connecting to [RUST] game server');
 
-      this.logger.debug('Connected to [RUST] game server!!!');
-
-      this.ws.on('message', (m: string) => {
-        this.listener(m);
+      this.ws.on('message', (m: Buffer) => {
+        this.listener(m.toString());
       });
 
       this.ws.on('open', () => {
@@ -86,40 +73,37 @@ export class RustEmitter extends TakaroEmitter {
     this.logger.debug('Websocket connection has been closed');
     return;
   }
-  private transform(obj: Record<string, unknown>): RustEvent {
-    // Currently this contains only lowercasing the keys.
-    return {
-      message: obj.Message as string,
-      identifier: obj.Identifier as number,
-      type: obj.Type as RustEventType,
-      stacktrace: obj.Stacktrace as string,
-    };
-  }
 
   async parseMessage(e: RustEvent) {
     // TODO: We probably want to handle the stacktrace first, because in that case it might be an invalid message.
     // TODO: Certain events have a different type. We could split the events based on these types to improve performance.
-    if (EventRegexMap[GameEvents.PLAYER_CONNECTED].test(e.message)) {
+    if (EventRegexMap[GameEvents.PLAYER_CONNECTED].test(e.Message)) {
       this.logger.debug('regexje');
-      const data = await this.handlePlayerConnected(e.message);
+      const data = await this.handlePlayerConnected(e);
       this.emit(GameEvents.PLAYER_CONNECTED, data);
     }
-    if (EventRegexMap[GameEvents.PLAYER_DISCONNECTED].test(e.message)) {
-      const data = await this.handlePlayerDisconnected(e.message);
+    if (EventRegexMap[GameEvents.PLAYER_DISCONNECTED].test(e.Message)) {
+      const data = await this.handlePlayerDisconnected(e);
       this.emit(GameEvents.PLAYER_DISCONNECTED, data);
+    }
+
+    if (e.Type === RustEventType.CHAT) {
+      const data = await this.handleChatMessage(e);
+      this.emit(GameEvents.CHAT_MESSAGE, data);
     }
 
     this.emit(
       GameEvents.LOG_LINE,
       await new EventLogLine().construct({
         timestamp: new Date(),
-        msg: e.message,
+        msg: e.Message,
       })
     );
   }
 
-  private async handlePlayerConnected(msg: string) {
+  private async handlePlayerConnected(e: RustEvent) {
     // "msg": "169.169.169.80:65384/76561198021481871/brunkel joined [windows/76561198021481871]"
+    const msg = e.Message;
     const expSearch =
       /(?<ip>[0-9\.]+):(?<port>\d{5})\/(?<steamId>\d{17})\/(?<name>.+) joined \[(?<device>.+)\/\d{17}\]/.exec(
         msg
@@ -139,13 +123,14 @@ export class RustEmitter extends TakaroEmitter {
         name: expSearch.groups.name,
         steamId: expSearch.groups.steamId,
         ip: expSearch.groups.ip,
-        device: expSearch.groups.device,
       });
 
       this.logger.debug('player: ', player);
 
       return new EventPlayerConnected().construct({
         player,
+        msg: msg,
+        timestamp: new Date(),
       });
     }
 
@@ -158,8 +143,9 @@ export class RustEmitter extends TakaroEmitter {
     throw new errors.GameServerError();
   }
 
-  private async handlePlayerDisconnected(msg: string) {
+  private async handlePlayerDisconnected(e: RustEvent) {
     // Example: 178.118.188.46:52210/76561198035925898/Emiel disconnecting: disconnect
+    const msg = e.Message;
     const expSearch =
       /(?<ip>.*):(?<port>\d{5})\/(?<platformId>\d{17})\/(?<name>.*) disconnecting: disconnect/.exec(
         msg
@@ -174,9 +160,14 @@ export class RustEmitter extends TakaroEmitter {
       const player = await new IGamePlayer().construct({
         name: expSearch.groups.name,
         steamId: expSearch.groups.steamId,
+        gameId: expSearch.groups.steamId,
       });
 
-      return new EventPlayerDisconnected().construct({ player });
+      return new EventPlayerDisconnected().construct({
+        player,
+        msg,
+        timestamp: new Date(),
+      });
     }
 
     this.logger.error(
@@ -188,12 +179,22 @@ export class RustEmitter extends TakaroEmitter {
     throw new errors.GameServerError();
   }
 
+  private async handleChatMessage(e: RustEvent): Promise<EventChatMessage> {
+    const parsed = JSON.parse(e.Message);
+
+    return new EventChatMessage().construct({
+      msg: parsed.Message,
+      player: await new IGamePlayer().construct({
+        gameId: parsed.UserId,
+        name: parsed.Username,
+      }),
+      timestamp: new Date(parsed.Time * 1000),
+    });
+  }
+
   async listener(data: string) {
     try {
-      data.replace(/"([^"]+)":/g, (_, $1: string) => {
-        return '"' + $1.toLowerCase() + '":';
-      });
-      const event = this.transform(JSON.parse(data));
+      const event = JSON.parse(data);
       await this.parseMessage(event);
       this.logger.debug('event: ', event);
     } catch (error) {
