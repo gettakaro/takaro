2f9e4275512603e76f99e5884f1c4bc359228aa5
Branco <branco.bruyneel@gmail.com>
2023-06-12 19:33:37 +0200

    chore: remove commandArguments component

---
+++ packages/web-main/src/components/modules/Editor/configs/style.ts
@@ -0,0 +1,41 @@
+import { Button, styled } from '@takaro/lib-components';
+
+export const StyledButton = styled(Button)`
+  margin-top: ${({ theme }) => theme.spacing[2]};
+`;
+
+export const StyledTest = styled.div`
+  width: 100%;
+  display: flex;
+`;
+
+export const ArgumentList = styled.ul`
+  width: 100%;
+  display: flex;
+  gap: ${({ theme }) => theme.spacing[4]};
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const ArgumentCard = styled.li`
+  width: 100%;
+  display: flex;
+  gap: ${({ theme }) => theme.spacing[1]};
+  align-items: center;
+  }
+`;
+
+export const ContentContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: ${({ theme }) => theme.spacing[4]};
+`;
+
+export const Flex = styled.div<{ direction?: 'row' | 'column' }>`
+  width: 100%;
+  display: flex;
+  flex-direction: ${({ direction }) => direction || 'row'};
+  align-items: center;
+  gap: ${({ theme }) => theme.spacing[1]};
+`;
