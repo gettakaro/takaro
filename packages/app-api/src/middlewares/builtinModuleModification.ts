a068975dd3c1055999a3818ac728961f2b930d10
Branco Bruyneel <branoc.bruyneel@gmail.com>
2023-06-25 16:21:27 +0200

    feat: add builtinModuleModification middleware

---
+++ packages/app-api/src/middlewares/builtinModuleModification.ts
@@ -0,0 +1,31 @@
+import { NextFunction, Response } from 'express';
+import { errors } from '@takaro/util';
+import { AuthenticatedRequest } from '../service/AuthService.js';
+import { ModuleService } from '../service/ModuleService.js';
+
+export async function builtinModuleModificationMiddleware(
+  req: AuthenticatedRequest,
+  _res: Response,
+  next: NextFunction
+): Promise<void> {
+  const moduleService = new ModuleService(req.domainId);
+
+  const type = req.path.split('/')[1];
+
+  let mod;
+
+  if (req.body.moduleId) {
+    mod = await moduleService.findOne(req.body.moduleId);
+  } else {
+    mod = await moduleService.findOneBy(
+      type,
+      req.body.commandId || req.params.id
+    );
+  }
+
+  if (mod?.builtin) {
+    return next(new errors.BadRequestError('Cannot modify builtin modules'));
+  }
+
+  next();
+}
