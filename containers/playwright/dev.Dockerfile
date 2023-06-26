2df642090008414e695bfa21caeb1546e5e70ce3
Emiel Van Severen <35073890+emielvanseveren@users.noreply.github.com>
2023-06-26 21:10:29 +0200

    wip: playwright

---
+++ containers/playwright/dev.Dockerfile
@@ -0,0 +1,19 @@
+FROM mcr.microsoft.com/playwright:v1.35.0-jammy
+
+RUN npm install -g npm@9
+
+WORKDIR /app
+
+COPY package*.json  ./
+COPY tsconfig*.json ./
+
+COPY nodemon.json   ./
+COPY jest.config.js ./
+COPY .mocharc.js    ./
+
+COPY packages/lib-apiclient           ./packages/lib-apiclient
+COPY packages/test                    ./packages/test
+COPY packages/e2e                     ./packages/e2e
+
+RUN npm ci 
+CMD ["npm", "run", "dev"]
---
+++ packages/e2e/playwright.config.ts
@@ -0,0 +1,47 @@
+import pkg from '@playwright/test';
+const { defineConfig, devices } = pkg;
+
+export default defineConfig({
+  // Look for test files in the "tests" directory, relative to this configuration file.
+  testDir: 'src',
+
+  // Run all tests in parallel.
+  fullyParallel: true,
+
+  // Fail the build on CI if you accidentally left test.only in the source code.
+  forbidOnly: !!process.env.CI,
+
+  // Retry on CI only.
+  retries: process.env.CI ? 2 : 0,
+
+  // Opt out of parallel tests on CI.
+  workers: process.env.CI ? 1 : undefined,
+
+  // Reporter to use
+  reporter: 'html',
+
+  use: {
+    // Base URL to use in actions like `await page.goto('/')`.
+    baseURL: 'http://0.0.0.0:20000',
+
+    // Collect trace when retrying the failed test.
+    trace: 'on-first-retry',
+  },
+  // Configure projects for major browsers.
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
+  ],
+});
