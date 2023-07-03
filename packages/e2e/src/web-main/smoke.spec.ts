a6c2ef9975f5535e88de3dd3ce156e69a2bb3cda
Emiel Van Severen <35073890+emielvanseveren@users.noreply.github.com>
2023-07-03 10:26:24 +0200

    chore: use simplier basetest which creates less data

--- packages/e2e/src/web-main/smoke.spec.ts
+++ packages/e2e/src/web-main/smoke.spec.ts
@@ -1,34 +1,34 @@
 import playwright from '@playwright/test';
-import { test } from './fixtures/index.js';
+import { basicTest } from './fixtures/index.js';
 
-const { test: pwTest, expect } = playwright;
+const { test, expect } = playwright;
 
-pwTest('has title', async ({ page }) => {
+test('has title', async ({ page }) => {
   await page.goto(`${process.env.TAKARO_FRONTEND_HOST}/`);
   await expect(page).toHaveTitle(/Takaro/);
 });
 
-test('Can go to dashboard', async ({ page }) => {
+basicTest('Can go to dashboard', async ({ page }) => {
   await page.getByRole('link', { name: 'Dashboard' }).click();
   await expect(page.getByRole('link', { name: 'Dashboard' })).toBeVisible();
 });
 
-test('Can go to servers', async ({ page }) => {
+basicTest('Can go to servers', async ({ page }) => {
   await page.getByRole('link', { name: 'Servers' }).click();
   await expect(page.getByRole('heading', { name: 'Servers' })).toBeVisible();
 });
 
-test('Can go to players', async ({ page }) => {
+basicTest('Can go to players', async ({ page }) => {
   await page.getByRole('link', { name: 'Players' }).click();
   await expect(page.getByRole('heading', { name: 'Players' })).toBeVisible();
 });
 
-test('Can go to modules', async ({ page }) => {
+basicTest('Can go to modules', async ({ page }) => {
   await page.getByRole('link', { name: 'Modules' }).click();
   await expect(page.getByRole('heading', { name: 'Modules' })).toBeVisible();
 });
 
-test('Can go to settings', async ({ page }) => {
+basicTest('Can go to settings', async ({ page }) => {
   await page.getByRole('link', { name: 'Settings' }).click();
   await expect(page.getByRole('heading', { name: 'Settings' })).toBeVisible();
 });
