56ed8c8aa1f98aed51ab118f055f9415fdc277f5
Emiel Van Severen <35073890+emielvanseveren@users.noreply.github.com>
2023-07-05 23:49:08 +0200

    Fix: get rid of double states and only base on value

--- packages/lib-components/src/components/inputs/Switch/Generic.tsx
+++ packages/lib-components/src/components/inputs/Switch/Generic.tsx
@@ -1,48 +1,51 @@
-import { useEffect, useState, forwardRef } from 'react';
+import { forwardRef, useRef } from 'react';
 import { getTransition } from '../../../helpers';
 import { Dot, ContentContainer } from './style';
-import { defaultInputProps, defaultInputPropsFactory, GenericInputPropsFunctionHandlers } from '../InputProps';
+import { defaultInputProps, defaultInputPropsFactory, GenericInputProps } from '../InputProps';
 import { setAriaDescribedBy } from '../layout';
 
-export type GenericSwitchProps = GenericInputPropsFunctionHandlers<boolean, HTMLDivElement>;
+export type GenericSwitchProps = GenericInputProps<boolean, HTMLInputElement>;
 
 const defaultsApplier = defaultInputPropsFactory<GenericSwitchProps>(defaultInputProps);
 
 export const GenericSwitch = forwardRef<HTMLButtonElement, GenericSwitchProps>((props, ref) => {
-  const { readOnly, onChange, value, id, hasDescription, name } = defaultsApplier(props);
-
-  const [isChecked, setChecked] = useState<boolean>(value as boolean);
+  const { readOnly, onChange, value: isChecked, id, hasDescription, name } = defaultsApplier(props);
+  const inputRef = useRef<HTMLInputElement>(null);
 
   function handleOnClick(): void {
-    setChecked(!isChecked);
+    inputRef.current?.click();
   }
 
-  useEffect(() => {
-    setChecked(value as boolean);
-  }, [value]);
-
-  useEffect(() => {
-    onChange(isChecked);
-  }, [isChecked]);
-
   return (
-    <ContentContainer
-      role="switch"
-      onClick={handleOnClick}
-      ref={ref}
-      id={id}
-      aria-describedby={setAriaDescribedBy(name, hasDescription)}
-      aria-checked={isChecked}
-      isChecked={isChecked}
-      type="button"
-    >
-      <Dot
-        animate={{ right: isChecked ? '3px' : '23px' }}
-        readOnly={readOnly}
-        isChecked={isChecked}
-        layout
-        transition={getTransition()}
+    <>
+      <input
+        type="checkbox"
+        aria-hidden="true"
+        style={{ display: 'none' }}
+        checked={isChecked}
+        tabIndex={-1}
+        value="on"
+        onChange={(e) => onChange(e)}
+        ref={inputRef}
       />
-    </ContentContainer>
+      <ContentContainer
+        role="switch"
+        onClick={handleOnClick}
+        ref={ref}
+        id={id}
+        aria-describedby={setAriaDescribedBy(name, hasDescription)}
+        aria-checked={isChecked}
+        isChecked={isChecked}
+        type="button"
+      >
+        <Dot
+          animate={{ right: isChecked ? '3px' : '23px' }}
+          readOnly={readOnly}
+          isChecked={isChecked}
+          layout
+          transition={getTransition()}
+        />
+      </ContentContainer>
+    </>
   );
 });
