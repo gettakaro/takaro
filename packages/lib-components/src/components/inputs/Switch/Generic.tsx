0cceb962ec34240109685bb2d417052164d101c2
Emiel Van Severen <35073890+emielvanseveren@users.noreply.github.com>
2023-07-05 23:32:42 +0200

    chore: improvem style of switch

--- packages/lib-components/src/components/inputs/Switch/Generic.tsx
+++ packages/lib-components/src/components/inputs/Switch/Generic.tsx
@@ -1,53 +1,48 @@
 import { useEffect, useState, forwardRef } from 'react';
 import { getTransition } from '../../../helpers';
-import { Dot, Line, ContentContainer } from './style';
-import {
-  defaultInputProps,
-  defaultInputPropsFactory,
-  GenericInputPropsFunctionHandlers,
-} from '../InputProps';
+import { Dot, ContentContainer } from './style';
+import { defaultInputProps, defaultInputPropsFactory, GenericInputPropsFunctionHandlers } from '../InputProps';
 import { setAriaDescribedBy } from '../layout';
 
-export type GenericSwitchProps = GenericInputPropsFunctionHandlers<
-  boolean,
-  HTMLDivElement
->;
+export type GenericSwitchProps = GenericInputPropsFunctionHandlers<boolean, HTMLDivElement>;
 
-const defaultsApplier =
-  defaultInputPropsFactory<GenericSwitchProps>(defaultInputProps);
+const defaultsApplier = defaultInputPropsFactory<GenericSwitchProps>(defaultInputProps);
 
-export const GenericSwitch = forwardRef<HTMLDivElement, GenericSwitchProps>(
-  (props, ref) => {
-    const { readOnly, onChange, value, id, hasDescription, name } =
-      defaultsApplier(props);
+export const GenericSwitch = forwardRef<HTMLButtonElement, GenericSwitchProps>((props, ref) => {
+  const { readOnly, onChange, value, id, hasDescription, name } = defaultsApplier(props);
 
-    const [isChecked, setChecked] = useState<boolean>(value as boolean);
+  const [isChecked, setChecked] = useState<boolean>(value as boolean);
 
-    function handleOnClick(): void {
-      setChecked(!isChecked);
-    }
-
-    useEffect(() => {
-      onChange(isChecked);
-    }, [isChecked]);
-
-    return (
-      <ContentContainer
-        onClick={handleOnClick}
-        ref={ref}
-        id={id}
-        aria-describedby={setAriaDescribedBy(name, hasDescription)}
-      >
-        <Line readOnly={readOnly} isChecked={isChecked}>
-          <Dot
-            animate={{ right: isChecked ? '-2px' : '15px' }}
-            readOnly={readOnly}
-            isChecked={isChecked}
-            layout
-            transition={getTransition()}
-          />
-        </Line>
-      </ContentContainer>
-    );
+  function handleOnClick(): void {
+    setChecked(!isChecked);
   }
-);
+
+  useEffect(() => {
+    setChecked(value as boolean);
+  }, [value]);
+
+  useEffect(() => {
+    onChange(isChecked);
+  }, [isChecked]);
+
+  return (
+    <ContentContainer
+      role="switch"
+      onClick={handleOnClick}
+      ref={ref}
+      id={id}
+      aria-describedby={setAriaDescribedBy(name, hasDescription)}
+      aria-checked={isChecked}
+      isChecked={isChecked}
+      type="button"
+    >
+      <Dot
+        animate={{ right: isChecked ? '3px' : '23px' }}
+        readOnly={readOnly}
+        isChecked={isChecked}
+        layout
+        transition={getTransition()}
+      />
+    </ContentContainer>
+  );
+});
