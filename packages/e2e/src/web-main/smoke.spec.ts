7dca0adc7da526e34353122c76d691eb65ce47ae
Emiel Van Severen <35073890+emielvanseveren@users.noreply.github.com>
2023-07-05 20:59:29 +0200

    Fix: smoketests

--- packages/e2e/src/web-main/smoke.spec.ts
+++ packages/e2e/src/web-main/smoke.spec.ts
@@ -11,25 +11,25 @@ test('has title', async ({ page }) => {
 
 basicTest('Can go to dashboard', async ({ page }) => {
   await page.getByRole('link', { name: 'Dashboard' }).click();
-  await expect(page.getByRole('link', { name: 'Dashboard', exact: true })).toBeVisible();
+  await expect(page.getByRole('heading', { name: 'dashboard', exact: true })).toBeVisible();
 });
 
 basicTest('Can go to servers', async ({ page }) => {
   await page.getByRole('link', { name: 'Servers' }).click();
-  await expect(page.getByRole('heading', { name: 'Servers', exact: true })).toBeVisible();
+  await expect(page.getByRole('heading', { name: 'servers', exact: true })).toBeVisible();
 });
 
 basicTest('Can go to players', async ({ page }) => {
   await page.getByRole('link', { name: 'Players' }).click();
-  await expect(page.getByRole('heading', { name: 'Players', exact: true })).toBeVisible();
+  await expect(page.getByRole('heading', { name: 'players', exact: true })).toBeVisible();
 });
 
 basicTest('Can go to modules', async ({ page }) => {
   await page.getByRole('link', { name: 'Modules' }).click();
-  await expect(page.getByRole('heading', { name: 'Modules', exact: true })).toBeVisible();
+  await expect(page.getByRole('heading', { name: 'modules', exact: true })).toBeVisible();
 });
 
 basicTest('Can go to settings', async ({ page }) => {
   await page.getByRole('link', { name: 'Settings', exact: true }).click();
-  await expect(page.getByRole('heading', { name: 'Settings' })).toBeVisible();
+  await expect(page.getByRole('heading', { name: 'settings' })).toBeVisible();
 });
