d5d5d140e7972235d261a01db76c2cd4343d6bf4
Emiel Van Severen <35073890+emielvanseveren@users.noreply.github.com>
2023-07-05 23:35:15 +0200

    chore: replace controlled with uncontrolled switch

--- packages/web-main/src/components/DiscordSettings/ServerCard.tsx
+++ packages/web-main/src/components/DiscordSettings/ServerCard.tsx
@@ -1,13 +1,18 @@
 import { GuildOutputDTO } from '@takaro/apiclient';
-import { Card, Switch, styled } from '@takaro/lib-components';
+import { Card, styled, UnControlledSwitch } from '@takaro/lib-components';
 import { useDiscordGuildUpdate } from 'queries/discord';
-import { FC, useEffect } from 'react';
-import { useForm } from 'react-hook-form';
+import { FC } from 'react';
 import { FaDiscord } from 'react-icons/fa';
 
 interface IServerCardProps {
   guild: GuildOutputDTO;
 }
+
+const Box = styled.div`
+  div {
+    margin-bottom: 0;
+  }
+`;
 
 const StyledCard = styled(Card)`
   display: grid;
@@ -20,39 +25,30 @@ const StyledIcon = styled.img`
 `;
 
 export const ServerCard: FC<IServerCardProps> = ({ guild }) => {
-  const { mutate, isError } = useDiscordGuildUpdate();
+  const { mutate } = useDiscordGuildUpdate();
 
-  const iconUrl = guild.icon
-    ? `https://cdn.discordapp.com/icons/${guild.discordId}/${guild.icon}.png?size=32`
-    : null;
-  const iconElement = iconUrl ? (
-    <StyledIcon src={iconUrl} alt={guild.name} />
-  ) : (
-    <FaDiscord size={32} />
-  );
+  const iconUrl = guild.icon ? `https://cdn.discordapp.com/icons/${guild.discordId}/${guild.icon}.png?size=32` : null;
+  const iconElement = iconUrl ? <StyledIcon src={iconUrl} alt={guild.name} /> : <FaDiscord size={32} />;
 
-  const { control, watch, setValue } = useForm({
-    defaultValues: {
-      takaroEnabled: guild.takaroEnabled,
-    },
-  });
-
-  const takaroEnabled = watch('takaroEnabled');
-
-  useEffect(() => {
-    mutate({ id: guild.id, input: { takaroEnabled } });
-  }, [takaroEnabled, guild.id, mutate]);
-
-  useEffect(() => {
-    setValue('takaroEnabled', guild.takaroEnabled);
-  }, [isError, guild.takaroEnabled, setValue]);
+  const handleOnSwitchChange = (val: boolean) => {
+    mutate({ id: guild.id, input: { takaroEnabled: val } });
+  };
 
   return (
     <StyledCard>
       {iconElement}
       <h2>{guild.name}</h2>
       <form>
-        <Switch control={control} name="takaroEnabled" />
+        <Box>
+          <UnControlledSwitch
+            hasDescription={false}
+            name="guildEnabled"
+            id={`guild-${guild.id}`}
+            value={guild.takaroEnabled}
+            onChange={handleOnSwitchChange}
+            hasError={false}
+          />
+        </Box>
       </form>
     </StyledCard>
   );
