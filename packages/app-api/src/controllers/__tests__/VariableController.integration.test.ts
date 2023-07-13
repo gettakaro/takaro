475b597bdfe2000b35956f3dc9eb5db947d72085
Niek Candaele <niek@candaele.dev>
2023-07-13 21:22:02 +0200

    feat: allow variables to be module-scoped

--- packages/app-api/src/controllers/__tests__/VariableController.integration.test.ts
+++ packages/app-api/src/controllers/__tests__/VariableController.integration.test.ts
@@ -1,5 +1,5 @@
 import { IntegrationTest, expect, integrationConfig, EventsAwaiter } from '@takaro/test';
-import { GameServerOutputDTO, PlayerOutputDTO, VariableOutputDTO } from '@takaro/apiclient';
+import { GameServerOutputDTO, ModuleOutputDTO, PlayerOutputDTO, VariableOutputDTO } from '@takaro/apiclient';
 import { config } from '../../config.js';
 import { EventTypes } from '@takaro/modules';
 
@@ -18,6 +18,7 @@ interface ISetupWithGameServersAndPlayers {
   gameServer1: GameServerOutputDTO;
   gameServer2: GameServerOutputDTO;
   players: PlayerOutputDTO[];
+  mod: ModuleOutputDTO;
 }
 
 const setupWithGameServersAndPlayers = async function (
@@ -39,6 +40,12 @@ const setupWithGameServersAndPlayers = async function (
     }),
   });
 
+  const mod = (
+    await this.client.module.moduleControllerCreate({
+      name: 'Test module',
+    })
+  ).data.data;
+
   const eventsAwaiter = new EventsAwaiter();
   await eventsAwaiter.connect(this.client);
   const connectedEvents = eventsAwaiter.waitForEvents(EventTypes.PLAYER_CONNECTED, 10);
@@ -56,6 +63,7 @@ const setupWithGameServersAndPlayers = async function (
     gameServer1: gameServer1.data.data,
     gameServer2: gameServer2.data.data,
     players,
+    mod,
   };
 };
 
@@ -320,6 +328,42 @@ const tests = [
       });
     },
     expectedStatus: 400,
+  }),
+  new IntegrationTest<ISetupWithGameServersAndPlayers>({
+    group,
+    snapshot: true,
+    name: 'Create with moduleId',
+    setup: setupWithGameServersAndPlayers,
+    test: async function () {
+      return this.client.variable.variableControllerCreate({
+        key: 'Test variable',
+        value: 'Test value',
+        moduleId: this.setupData.mod.id,
+      });
+    },
+    filteredFields: ['moduleId'],
+  }),
+  new IntegrationTest<ISetupWithGameServersAndPlayers>({
+    group,
+    snapshot: true,
+    name: 'Prevents creating duplicate variables with same moduleId',
+    setup: setupWithGameServersAndPlayers,
+    test: async function () {
+      // First creation should succeed
+      await this.client.variable.variableControllerCreate({
+        key: 'Test variable',
+        value: 'Test value',
+        moduleId: this.setupData.mod.id,
+      });
+
+      // Second creation with same moduleId should fail
+      return this.client.variable.variableControllerCreate({
+        key: 'Test variable',
+        value: 'Test value',
+        moduleId: this.setupData.mod.id,
+      });
+    },
+    expectedStatus: 409, // Expect a conflict HTTP status code
   }),
 ];
 
