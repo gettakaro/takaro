1a971a388890aa27db66a4dbbcd98931e2f82443
Emiel Van Severen <35073890+emielvanseveren@users.noreply.github.com>
2023-07-17 00:59:29 +0200

    chore: improve design of players table

--- packages/web-main/src/pages/Players.tsx
+++ packages/web-main/src/pages/Players.tsx
@@ -1,9 +1,17 @@
 import { FC, Fragment } from 'react';
 import { Helmet } from 'react-helmet';
-import { styled, Table, Loading, useTableActions } from '@takaro/lib-components';
+import { styled, Table, Loading, useTableActions, IconButton, Dropdown } from '@takaro/lib-components';
 import { PlayerOutputDTO, PlayerSearchInputDTOSortDirectionEnum } from '@takaro/apiclient';
 import { createColumnHelper } from '@tanstack/react-table';
 import { usePlayers } from 'queries/players';
+import { useNavigate } from 'react-router-dom';
+import { PATHS } from 'paths';
+import {
+  AiOutlineUser as ProfileIcon,
+  AiOutlineEdit as EditIcon,
+  AiOutlineDelete as DeleteIcon,
+  AiOutlineMenu as MenuIcon,
+} from 'react-icons/ai';
 
 const TableContainer = styled.div`
   width: 100%;
@@ -14,6 +22,7 @@ const TableContainer = styled.div`
 
 const Players: FC = () => {
   const { pagination, columnFilters, sorting, columnSearch } = useTableActions<PlayerOutputDTO>();
+  const navigate = useNavigate();
 
   const { data, isLoading } = usePlayers({
     page: pagination.paginationState.pageIndex,
@@ -59,12 +68,14 @@ const Players: FC = () => {
       id: 'epicOnlineServicesId',
       cell: (info) => info.getValue(),
       enableColumnFilter: true,
+      enableSorting: true,
     }),
     columnHelper.accessor('xboxLiveId', {
       header: 'Xbox ID',
       id: 'xboxLiveId',
       cell: (info) => info.getValue(),
       enableColumnFilter: true,
+      enableSorting: true,
     }),
     columnHelper.accessor('createdAt', {
       header: 'Created at',
@@ -79,6 +90,38 @@ const Players: FC = () => {
       cell: (info) => info.getValue(),
       enableColumnFilter: true,
       enableSorting: true,
+    }),
+
+    columnHelper.display({
+      header: 'Actions',
+      id: 'actions',
+      enableSorting: false,
+      enableColumnFilter: false,
+      enableHiding: false,
+      enablePinning: false,
+      enableGlobalFilter: false,
+      enableResizing: false,
+      maxSize: 50,
+
+      cell: (info) => (
+        <Dropdown>
+          <Dropdown.Trigger asChild>
+            <IconButton icon={<MenuIcon />} ariaLabel="player-actions" />
+          </Dropdown.Trigger>
+          <Dropdown.Menu>
+            <Dropdown.Menu.Group divider>
+              <Dropdown.Menu.Item
+                label="Go to player profile"
+                icon={<ProfileIcon />}
+                onClick={() => navigate(`${PATHS.players()}/${info.row.original.id}`)}
+              />
+              <Dropdown.Menu.Item label="go to user profile" icon={<EditIcon />} onClick={() => navigate('')} />
+            </Dropdown.Menu.Group>
+            <Dropdown.Menu.Item label="Edit roles" icon={<EditIcon />} onClick={() => navigate('')} />
+            <Dropdown.Menu.Item label="Ban player" icon={<DeleteIcon />} onClick={() => navigate('')} />
+          </Dropdown.Menu>
+        </Dropdown>
+      ),
     }),
   ];
 
