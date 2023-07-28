ed3863870a6e9401ed520a61df3c9e45b0e77a5b
Niek Candaele <niek@candaele.dev>
2023-07-28 10:33:08 +0200

    fix: pagination in ory

--- packages/lib-auth/src/lib/paginationHelpers.ts
+++ packages/lib-auth/src/lib/paginationHelpers.ts
@@ -1,11 +1,11 @@
 import { IdentityApi } from '@ory/client';
 
-export async function* paginateIdentities(adminClient: IdentityApi, page = 1, perPage = 100) {
-  let currentPage = page;
+export async function* paginateIdentities(adminClient: IdentityApi, page = undefined, perPage = 1) {
+  let nextPage = page;
 
   while (true) {
     const response = await adminClient.listIdentities({
-      page: currentPage,
+      page: nextPage,
       perPage,
     });
 
@@ -16,6 +16,18 @@ export async function* paginateIdentities(adminClient: IdentityApi, page = 1, pe
 
     yield response.data;
 
-    currentPage++;
+    // Parse Link header
+    const linkHeader = response.headers.link;
+    const links = linkHeader.split(',');
+
+    const nextLink = links.find((link: string) => link.includes('rel="next"'));
+
+    if (nextLink) {
+      // Extract the 'next' page URL
+      const match = nextLink.match(/<(.*)>/);
+      nextPage = match ? match[1] : undefined;
+    } else {
+      break; // stop if there is no next page
+    }
   }
 }
