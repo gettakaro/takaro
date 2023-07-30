a94ab7d2865f45bf81abcc8c0f4901cc60d852a8
Niek Candaele <niek@candaele.dev>
2023-07-30 20:10:17 +0200

    feat: expose logs from inside module

--- packages/app-vmm/src/service/workers/executeLocal.ts
+++ packages/app-vmm/src/service/workers/executeLocal.ts
@@ -1,44 +1,47 @@
 import { getTakaro } from '@takaro/helpers';
-import vm, { Module } from 'node:vm';
+import vm from 'node:vm';
 import { config } from '../../config.js';
 import axios from 'axios';
 import * as _ from 'lodash-es';
+import { FunctionExecutor, ILog } from './executeFunction.js';
 
 /**
  * !!!!!!!!!!!!!!!!!!!!! node:vm is not secure, don't use this in production !!!!!!!!!!!!!!!!!
  */
-
-export async function executeFunctionLocal(fn: string, data: Record<string, unknown>, token: string) {
+export const executeFunctionLocal: FunctionExecutor = async (
+  fn: string,
+  data: Record<string, unknown>,
+  token: string
+) => {
   data.token = token;
   data.url = config.get('takaro.url');
 
-  const logs = {
-    stdout: [] as string[],
-    stderr: [] as string[],
-  };
+  const logs: ILog[] = [];
 
-  const contextifiedObject = vm.createContext({
-    process: { env: { DATA: JSON.stringify(data) } },
-    console: {
-      log: (...args: string[]) => {
-        logs.stdout.push(...args);
-      },
-      error: (...args: string[]) => {
-        logs.stderr.push(...args);
-      },
-    },
-  });
-
-  const toEval = new vm.SourceTextModule(fn, { context: contextifiedObject });
+  const toEval = new vm.SourceTextModule(fn);
   const monkeyPatchedGetData = () => {
     return data;
   };
 
-  await toEval.link((specifier: string, referencingModule: Module) => {
+  const monkeyPatchedGetTakaro = function () {
+    function pushLog(...args: string[]) {
+      logs.push({ msg: args[0], details: { args: args.slice(1) } });
+    }
+    const patchedConsole = {
+      log: pushLog,
+      error: pushLog,
+      warn: pushLog,
+      info: pushLog,
+      debug: pushLog,
+    };
+    return getTakaro(data, patchedConsole);
+  };
+
+  await toEval.link((specifier: string, referencingModule) => {
     const syntheticHelpersModule = new vm.SyntheticModule(
       ['getTakaro', 'getData', 'axios', '_', 'lodash'],
       function () {
-        this.setExport('getTakaro', getTakaro);
+        this.setExport('getTakaro', monkeyPatchedGetTakaro);
         this.setExport('getData', monkeyPatchedGetData);
         this.setExport('axios', axios);
         this.setExport('_', _);
@@ -61,11 +64,15 @@ export async function executeFunctionLocal(fn: string, data: Record<string, unkn
       success: true,
     };
   } catch (error) {
-    if (error instanceof Error) logs.stderr.push(error.message);
+    if (error instanceof Error)
+      logs.push({
+        msg: error.message,
+        details: error.stack,
+      });
 
     return {
       logs,
       success: false,
     };
   }
-}
+};
