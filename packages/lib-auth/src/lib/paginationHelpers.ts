82228c2784e106cff74895f2439e4fc67e77895d
Niek Candaele <niek@candaele.dev>
2023-07-30 11:55:49 +0200

    fix: issue with ory pagination

--- packages/lib-auth/src/lib/paginationHelpers.ts
+++ packages/lib-auth/src/lib/paginationHelpers.ts
@@ -1,7 +1,8 @@
 import { IdentityApi } from '@ory/client';
+import { parse, URLSearchParams } from 'url';
 
 export async function* paginateIdentities(adminClient: IdentityApi, page = undefined, perPage = 100) {
-  let nextPage = page;
+  let nextPage: number | undefined = page;
 
   while (true) {
     const response = await adminClient.listIdentities({
@@ -19,13 +20,25 @@ export async function* paginateIdentities(adminClient: IdentityApi, page = undef
     // Parse Link header
     const linkHeader = response.headers.link;
     const links = linkHeader.split(',');
-
     const nextLink = links.find((link: string) => link.includes('rel="next"'));
 
-    if (nextLink) {
-      // Extract the 'next' page URL
-      const match = nextLink.match(/<(.*)>/);
-      nextPage = match ? match[1] : undefined;
+    if (!nextLink) {
+      break;
+    }
+
+    // Extract the 'next' page URL
+    const match = nextLink.match(/<(.*)>/);
+    const url = match ? match[1] : undefined;
+
+    if (!url) {
+      break;
+    }
+
+    const parsedUrl = parse(url);
+    const params = new URLSearchParams(parsedUrl.query || '');
+    const nextPageToken = params.get('page');
+    if (nextPageToken) {
+      nextPage = parseInt(nextPageToken, 10);
     } else {
       break; // stop if there is no next page
     }
