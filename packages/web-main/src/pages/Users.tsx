772d874cf0cb02715ea373f95451251ec6e4e67e
Emiel Van Severen <35073890+emielvanseveren@users.noreply.github.com>
2023-07-31 01:19:12 +0200

    Feat: add renderer for table toolbar (#518)

--- packages/web-main/src/pages/Users.tsx
+++ packages/web-main/src/pages/Users.tsx
@@ -124,12 +124,12 @@ const Users: FC = () => {
       <Helmet>
         <title>Users - Takaro</title>
       </Helmet>
-      <InviteUser />
       <TableContainer>
         <Table
           id="users"
           columns={columnDefs}
           data={data.data}
+          renderToolbar={() => <InviteUser />}
           pagination={{
             ...pagination,
             pageOptions: pagination.getPageOptions(data),
