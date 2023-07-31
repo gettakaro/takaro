e1be9f58d4840f9341b2ae767d7850a90b190453
Emiel Van Severen <35073890+emielvanseveren@users.noreply.github.com>
2023-07-31 12:31:06 +0200

    Feat: bunch of table improvements (#523)
    * Fix: types with filtering + Fix: issue with columns not showing when no rows
    
    * Feat: add empty state to table

--- packages/lib-components/src/components/data/Table/index.tsx
+++ packages/lib-components/src/components/data/Table/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { Dispatch, SetStateAction, useEffect, useState } from 'react';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import { Density } from '../../../styled';
@@ -11,19 +11,20 @@ import {
   useReactTable,
   ColumnDef,
   SortingState,
-  ColumnFiltersState,
   ColumnOrderState,
   VisibilityState,
   ColumnPinningState,
   RowSelectionState,
 } from '@tanstack/react-table';
 import { Wrapper, StyledTable, Toolbar, PaginationContainer, Flex } from './style';
-import { ToggleButtonGroup } from '../../../components';
+import { Empty, ToggleButtonGroup } from '../../../components';
 import { AiOutlinePicCenter as RelaxedDensityIcon, AiOutlinePicRight as TightDensityIcon } from 'react-icons/ai';
 import { ColumnHeader, ColumnVisibility, Filter, Pagination } from './subcomponents';
-import { PageOptions } from '../../../hooks/useTableActions';
+import { ColumnFilter, PageOptions } from '../../../hooks/useTableActions';
 import { GenericCheckBox as CheckBox } from '../../inputs/CheckBox/Generic';
 import { useLocalStorage } from '../../../hooks';
+
+const ROW_SELECTION_COL_SPAN = 1;
 
 export interface TableProps<DataType extends object> {
   id: string;
@@ -50,12 +51,12 @@ export interface TableProps<DataType extends object> {
     pageOptions: PageOptions;
   };
   columnFiltering: {
-    columnFiltersState: ColumnFiltersState;
-    setColumnFiltersState: OnChangeFn<ColumnFiltersState>;
+    columnFiltersState: ColumnFilter[];
+    setColumnFiltersState: Dispatch<SetStateAction<ColumnFilter[]>>;
   };
   columnSearch: {
-    columnSearchState: ColumnFiltersState;
-    setColumnSearchState: OnChangeFn<ColumnFiltersState>;
+    columnSearchState: ColumnFilter[];
+    setColumnSearchState: OnChangeFn<ColumnFilter[]>;
   };
 }
 
@@ -133,8 +134,8 @@ export function Table<DataType extends object>({
     onColumnVisibilityChange: setColumnVisibility,
     onSortingChange: sorting?.setSortingState,
     onPaginationChange: pagination?.setPaginationState,
-    onColumnFiltersChange: columnFiltering?.setColumnFiltersState,
-    onGlobalFilterChange: columnSearch?.setColumnSearchState,
+    onColumnFiltersChange: (filters) => columnFiltering?.setColumnFiltersState(filters as ColumnFilter[]),
+    onGlobalFilterChange: (filters) => columnSearch?.setColumnSearchState(filters as ColumnFilter[]),
     onColumnOrderChange: setColumnOrder,
     onColumnPinningChange: setColumnPinning,
     onRowSelectionChange: rowSelection?.setRowSelectionState,
@@ -215,6 +216,17 @@ export function Table<DataType extends object>({
             ))}
           </thead>
           <tbody>
+            {/* empty state */}
+            {table.getRowModel().rows.length === 0 && (
+              <tr>
+                <td colSpan={table.getAllColumns().length + ROW_SELECTION_COL_SPAN}>
+                  <div style={{ width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                    <Empty header="No data" description="We couldn't find what you are looking for." actions={[]} />
+                  </div>
+                </td>
+              </tr>
+            )}
+
             {table.getRowModel().rows.map((row) => (
               <tr key={row.id}>
                 {row.getCanSelect() && (
@@ -242,31 +254,38 @@ export function Table<DataType extends object>({
               </tr>
             ))}
           </tbody>
-          <tfoot>
-            <tr>
-              {pagination && (
-                <td colSpan={columns.length + 1 /* +1 here is because we have an extra column for the selection */}>
-                  <PaginationContainer>
-                    <span>
-                      showing {table.getState().pagination.pageIndex * table.getState().pagination.pageSize + 1}-
-                      {table.getState().pagination.pageIndex * table.getState().pagination.pageSize +
-                        table.getRowModel().rows.length}{' '}
-                      of {pagination.pageOptions.total} entries
-                    </span>
-                    <Pagination
-                      pageCount={table.getPageCount()}
-                      hasNext={table.getCanNextPage()}
-                      hasPrevious={table.getCanPreviousPage()}
-                      previousPage={table.previousPage}
-                      nextPage={table.nextPage}
-                      pageIndex={table.getState().pagination.pageIndex}
-                      setPageIndex={table.setPageIndex}
-                    />
-                  </PaginationContainer>
-                </td>
-              )}
-            </tr>
-          </tfoot>
+          {table.getRowModel().rows.length > 1 && (
+            <tfoot>
+              <tr>
+                {pagination && (
+                  <td
+                    colSpan={
+                      columns.length +
+                      ROW_SELECTION_COL_SPAN /* +1 here is because we have an extra column for the selection */
+                    }
+                  >
+                    <PaginationContainer>
+                      <span>
+                        showing {table.getState().pagination.pageIndex * table.getState().pagination.pageSize + 1}-
+                        {table.getState().pagination.pageIndex * table.getState().pagination.pageSize +
+                          table.getRowModel().rows.length}{' '}
+                        of {pagination.pageOptions.total} entries
+                      </span>
+                      <Pagination
+                        pageCount={table.getPageCount()}
+                        hasNext={table.getCanNextPage()}
+                        hasPrevious={table.getCanPreviousPage()}
+                        previousPage={table.previousPage}
+                        nextPage={table.nextPage}
+                        pageIndex={table.getState().pagination.pageIndex}
+                        setPageIndex={table.setPageIndex}
+                      />
+                    </PaginationContainer>
+                  </td>
+                )}
+              </tr>
+            </tfoot>
+          )}
         </StyledTable>
       </DndProvider>
     </Wrapper>
--- packages/web-main/src/pages/Players.tsx
+++ packages/web-main/src/pages/Players.tsx
@@ -32,30 +32,18 @@ const Players: FC = () => {
       ? PlayerSearchInputDTOSortDirectionEnum.Desc
       : PlayerSearchInputDTOSortDirectionEnum.Asc,
     filters: {
-      name: [columnFilters.columnFiltersState.find((filter) => filter.id === 'name')?.value].filter(
-        Boolean
-      ) as string[],
-      steamId: [columnFilters.columnFiltersState.find((filter) => filter.id === 'steamId')?.value].filter(Boolean) as [
-        string
-      ],
-      epicOnlineServicesId: [
-        columnFilters.columnFiltersState.find((filter) => filter.id === 'epicOnlineServicesId')?.value,
-      ].filter(Boolean) as string[],
-      xboxLiveId: [columnFilters.columnFiltersState.find((filter) => filter.id === 'xboxLiveId')?.value].filter(
-        Boolean
-      ) as string[],
+      name: columnFilters.columnFiltersState.find((filter) => filter.id === 'name')?.value,
+      steamId: columnFilters.columnFiltersState.find((filter) => filter.id === 'steamId')?.value,
+      epicOnlineServicesId: columnFilters.columnFiltersState.find((filter) => filter.id === 'epicOnlineServicesId')
+        ?.value,
+      xboxLiveId: columnFilters.columnFiltersState.find((filter) => filter.id === 'xboxLiveId')?.value,
     },
     search: {
-      name: [columnSearch.columnSearchState.find((search) => search.id === 'name')?.value].filter(Boolean) as string[],
-      steamId: [columnSearch.columnSearchState.find((search) => search.id === 'steamId')?.value].filter(
-        Boolean
-      ) as string[],
-      epicOnlineServicesId: [
-        columnSearch.columnSearchState.find((search) => search.id === 'epicOnlineServicesId')?.value,
-      ].filter(Boolean) as string[],
-      xboxLiveId: [columnSearch.columnSearchState.find((search) => search.id === 'xboxLiveId')?.value].filter(
-        Boolean
-      ) as string[],
+      name: columnSearch.columnSearchState.find((search) => search.id === 'name')?.value,
+      steamId: columnSearch.columnSearchState.find((search) => search.id === 'steamId')?.value,
+      epicOnlineServicesId: columnSearch.columnSearchState.find((search) => search.id === 'epicOnlineServicesId')
+        ?.value,
+      xboxLiveId: columnSearch.columnSearchState.find((search) => search.id === 'xboxLiveId')?.value,
     },
   });
 
