1b0c170753aaf6e7c99417420f2e8a9542836fb8
Emiel Van Severen <35073890+emielvanseveren@users.noreply.github.com>
2023-07-31 13:40:35 +0200

    styles: improve tab style + add extra story (#526)

--- packages/lib-components/src/components/navigation/Tabs/Tabs.stories.tsx
+++ packages/lib-components/src/components/navigation/Tabs/Tabs.stories.tsx
@@ -1,19 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Meta, StoryFn } from '@storybook/react';
 import { Tabs, TabsProps } from '.';
 import { styled } from '../../../styled';
+import { Empty, IconButton, Popover } from '../../../components';
+import { AiOutlineTag as TagIcon } from 'react-icons/ai';
 
 const Wrapper = styled.div`
   background-color: ${({ theme }) => theme.colors.background};
-`;
-
-const EditorContent = styled.div`
-  padding: ${({ theme }) => theme.spacing['2']};
-`;
-
-const Container = styled.div`
-  border: 1px solid ${({ theme }) => theme.colors.secondary};
-  border-radius: ${({ theme }) => theme.borderRadius.large};
 `;
 
 export default {
@@ -42,6 +35,54 @@ export const UnControlled: StoryFn<TabsProps> = () => (
     </Tabs>
   </Wrapper>
 );
+
+const InnerContent = styled.div`
+  min-width: 600px;
+`;
+
+export const PopOver: StoryFn<TabsProps> = () => {
+  const [open, setOpen] = useState<boolean>(false);
+
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      <Popover.Trigger asChild>
+        <IconButton icon={<TagIcon />} onClick={() => setOpen(!open)} ariaLabel="open popover" />
+      </Popover.Trigger>
+      <Popover.Content>
+        <Tabs defaultValue="tab1">
+          <Tabs.List>
+            <Tabs.Trigger value="tab1">Local</Tabs.Trigger>
+            <Tabs.Trigger value="tab2">Lambda</Tabs.Trigger>
+          </Tabs.List>
+          <Tabs.Content value="tab1">
+            <InnerContent>
+              <Empty
+                header="No local environment set up"
+                description="You can set up a local environment by running the following command in your terminal:"
+                actions={[]}
+              />
+            </InnerContent>
+          </Tabs.Content>
+          <Tabs.Content value="tab2">
+            <InnerContent>
+              <h3>Content 2</h3>
+              <p>some long content here</p>
+            </InnerContent>
+          </Tabs.Content>
+        </Tabs>
+      </Popover.Content>
+    </Popover>
+  );
+};
+
+const EditorContent = styled.div`
+  padding: ${({ theme }) => theme.spacing['2']};
+`;
+
+const Container = styled.div`
+  border: 1px solid ${({ theme }) => theme.colors.secondary};
+  border-radius: ${({ theme }) => theme.borderRadius.large};
+`;
 
 export const Editor: StoryFn<TabsProps> = () => {
   return (
