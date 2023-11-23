91c66f9676f96596089a8154052cf749da16195b
Emiel Van Severen <35073890+emielvanseveren@users.noreply.github.com>
2023-11-23 16:05:10 +0100

    chore: move helpers + add new navigateTo helper

--- packages/e2e/src/web-main/helpers.ts
+++ packages/e2e/src/web-main/helpers.ts
@@ -1,6 +1,7 @@
 import playwright, { Page } from '@playwright/test';
 import { AdminClient } from '@takaro/apiclient';
 import { integrationConfig } from '@takaro/test';
+import { TEST_IDS } from './testIds.js';
 
 const { expect } = playwright;
 
@@ -14,6 +15,26 @@ export async function login(page: Page, username: string, password: string) {
   await page.getByLabel('PasswordRequired').fill(password);
   await page.getByRole('button', { name: 'Log in with Email' }).click();
   await expect(page.getByRole('link', { name: 'Takaro' })).toBeVisible();
+}
+
+type toOptions =
+  | 'global-settings'
+  | 'global-servers'
+  | 'global-events'
+  | 'global-players'
+  | 'global-users'
+  | 'global-modules'
+  | 'global-variables'
+  | 'global-roles'
+  | 'global-dashboard'
+  | 'server-settings'
+  | 'server-dashboard'
+  | 'server-modules';
+export async function navigateTo(page: Page, to: toOptions) {
+  const [n, opt] = to.split('-');
+  const nav = n === 'global' ? page.getByTestId(TEST_IDS.GLOBAL_NAV) : page.getByTestId(TEST_IDS.SERVER_NAV);
+  const link = opt.charAt(0).toUpperCase() + opt.slice(1);
+  await nav.getByRole('link', { name: link }).click();
 }
 
 export const getAdminClient = () => {
