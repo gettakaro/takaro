d80083a35e758be4c989a51bf38e805db656a6b1
Emiel Van Severen <35073890+emielvanseveren@users.noreply.github.com>
2023-12-05 12:35:16 +0100

    Add migration for new player events

---
+++ packages/lib-db/src/migrations/sql/20231205104953-add-new-player-hook-events.ts
@@ -0,0 +1,22 @@
+import { Knex } from 'knex';
+import { formatAlterTableEnumSql } from '../util/alterEnum.js';
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.raw(
+    formatAlterTableEnumSql('hooks', 'eventType', [
+      'log',
+      'player-connected',
+      'player-disconnected',
+      'chat-message',
+      'discord-message',
+      'player-death',
+      'entity-killed',
+    ])
+  );
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.raw(
+    formatAlterTableEnumSql('hooks', 'eventType', ['log', 'player-connected', 'player-disconnected', 'discord-message'])
+  );
+}
