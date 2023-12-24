50301e1bfe7613149322ede9ace08ccd245f0bde
Niek Candaele <22315101+niekcandaele@users.noreply.github.com>
2023-12-24 14:46:20 +0100

    feat: better styling for chat and online players cards (#785)
    * feat: better styling for chat and online players cards
    
    * feat: auto update online players card

--- packages/web-main/src/pages/gameserver/cards/ChatMessages.tsx
+++ packages/web-main/src/pages/gameserver/cards/ChatMessages.tsx
@@ -6,14 +6,14 @@ import {
 import { Loading, styled } from '@takaro/lib-components';
 import { useSelectedGameServer } from 'hooks/useSelectedGameServerContext';
 import { useSocket } from 'hooks/useSocket';
+import { PATHS } from 'paths';
 import { useEvents } from 'queries/events';
 import { FC, useEffect } from 'react';
 
 const SteamAvatar = styled.img`
   width: 2rem;
-  height: 100%;
+  height: 2rem;
   border-radius: 50%;
-  margin: auto;
 `;
 
 const ChatContainer = styled.div`
@@ -23,18 +23,21 @@ const ChatContainer = styled.div`
 `;
 
 const Message = styled.span`
-  display: block;
-  padding: 5px;
   border-bottom: 1px solid ${({ theme }) => theme.colors.backgroundAlt};
-  height: 2rem;
-  & > * {
-    margin-right: 1rem;
-  }
+  display: grid;
+  grid-template-columns: 1.5fr 2fr 5fr;
 `;
 
 const PlayerName = styled.span`
   font-weight: bold;
   margin-right: 10px;
+  margin-left: 10px;
+`;
+
+const PlayerContainer = styled.span`
+  display: grid;
+  grid-template-columns: 2rem 1fr;
+  align-items: center;
 `;
 
 const ChatMessage: FC<{ chatMessage: EventOutputDTO }> = ({ chatMessage }) => {
@@ -48,8 +51,12 @@ const ChatMessage: FC<{ chatMessage: EventOutputDTO }> = ({ chatMessage }) => {
   return (
     <Message>
       <span>{friendlyTimeStamp}</span>
-      <SteamAvatar src={avatarUrl} />
-      <PlayerName>{chatMessage.player?.name}</PlayerName>
+      <a href={PATHS.player.profile(chatMessage.player?.id as string)}>
+        <PlayerContainer>
+          <SteamAvatar src={avatarUrl} />
+          <PlayerName>{chatMessage.player?.name}</PlayerName>
+        </PlayerContainer>
+      </a>
       <span>{chatMessage.meta.message as string}</span>
     </Message>
   );
