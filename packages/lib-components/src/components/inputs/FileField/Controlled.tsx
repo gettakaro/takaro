7ab86825a17aa4549d7f565ac797d50d14423417
Emiel Van Severen <35073890+emielvanseveren@users.noreply.github.com>
2023-12-28 09:49:19 +0100

    Feat: <FileField/> (#803)

---
+++ packages/lib-components/src/components/inputs/FileField/Controlled.tsx
@@ -0,0 +1,104 @@
+import { FC, useState } from 'react';
+import { useController } from 'react-hook-form';
+import { ControlledInputProps, defaultInputProps, defaultInputPropsFactory } from '../InputProps';
+import { FileFieldProps, GenericFileField } from '.';
+import { Container, InputContainer } from './style';
+import { Label, ErrorMessage, Wrapper, Description } from '../layout';
+
+export type ControlledFileFieldProps = ControlledInputProps & FileFieldProps;
+
+const defaultsApplier = defaultInputPropsFactory<ControlledFileFieldProps>(defaultInputProps);
+
+export const ControlledFileField: FC<ControlledFileFieldProps> = (props) => {
+  const {
+    description,
+    loading,
+    label,
+    hint,
+    disabled,
+    required,
+    name,
+    size,
+    readOnly,
+    multiple = false,
+    placeholder,
+    control,
+  } = defaultsApplier(props);
+
+  const { field, fieldState } = useController({
+    name,
+    control,
+  });
+
+  const [showError, setShowError] = useState<boolean>(false);
+
+  const handleOnFocus = () => {
+    setShowError(true);
+  };
+
+  const handleOnBlur = () => {
+    field.onBlur();
+    setShowError(false);
+  };
+
+  if (loading) {
+    return (
+      <Wrapper>
+        <Container>
+          {label && (
+            <Label
+              required={required}
+              htmlFor={name}
+              error={!!fieldState.error}
+              size={size}
+              disabled={disabled}
+              text={label}
+              position="top"
+            />
+          )}
+          <InputContainer className="placeholder" />
+        </Container>
+      </Wrapper>
+    );
+  }
+
+  return (
+    <Wrapper>
+      <Container>
+        {label && (
+          <Label
+            required={required}
+            hint={hint}
+            htmlFor={name}
+            error={!!fieldState.error}
+            size={size}
+            text={label}
+            disabled={disabled}
+            position="top"
+          />
+        )}
+        <GenericFileField
+          hasError={!!fieldState.error}
+          disabled={disabled}
+          name={name}
+          id={name}
+          required={required}
+          size={size}
+          multiple={multiple}
+          readOnly={readOnly}
+          onChange={field.onChange}
+          onFocus={handleOnFocus}
+          onBlur={handleOnBlur}
+          placeholder={placeholder}
+          value={field.value}
+          ref={field.ref}
+          hasDescription={!!description}
+        />
+        {fieldState.error && fieldState.error.message && showError && (
+          <ErrorMessage message={fieldState.error.message} />
+        )}
+      </Container>
+      {description && <Description description={description} inputName={name} />}
+    </Wrapper>
+  );
+};
---
+++ packages/lib-components/src/components/inputs/FileField/index.tsx
@@ -0,0 +1,5 @@
+export { GenericFileField } from './Generic';
+export type { GenericFileFieldProps, FileFieldProps } from './Generic';
+
+export { ControlledFileField } from './Controlled';
+export type { ControlledFileFieldProps } from './Controlled';
