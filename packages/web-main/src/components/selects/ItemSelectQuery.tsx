154b9284ac81ed83e50d3bfc0616bb8e67f0d01e
Emiel Van Severen <github@vanseveren.dev>
2024-01-05 18:33:51 +0100

    Feat: ItemSelectQuery

---
+++ packages/web-main/src/components/selects/ItemSelectQuery.tsx
@@ -0,0 +1,106 @@
+import { GameServerOutputDTO, GameServerOutputDTOTypeEnum, ItemsOutputDTO } from '@takaro/apiclient';
+import { Avatar, getInitials, SelectQueryField, styled } from '@takaro/lib-components';
+import { useGameServer } from 'queries/gameservers';
+import { useItems } from 'queries/items';
+import { FC, useState } from 'react';
+import { CustomQuerySelectProps } from '.';
+
+interface ItemsSelectProps extends CustomQuerySelectProps {
+  gameServerId: string;
+}
+
+const Inner = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  width: 100%;
+
+  span {
+    margin-left: ${({ theme }) => theme.spacing['1']};
+  }
+`;
+
+const gameServerTypeToIconFolderMap = {
+  [GameServerOutputDTOTypeEnum.Mock]: 'rust',
+  [GameServerOutputDTOTypeEnum.Rust]: 'rust',
+  [GameServerOutputDTOTypeEnum.Sevendaystodie]: '7d2d',
+};
+
+export const ItemSelect: FC<ItemsSelectProps> = ({
+  control,
+  name,
+  hint,
+  size,
+  label,
+  loading,
+  disabled,
+  inPortal,
+  readOnly,
+  required,
+  hasMargin,
+  description,
+  placeholder,
+  gameServerId,
+}) => {
+  const [itemName, setItemName] = useState<string>('');
+
+  const { data: gameServer, isLoading: isLoadingGameServer } = useGameServer(gameServerId);
+  const { data, isLoading: isLoadingItems } = useItems(
+    { search: { name: [itemName] }, filters: { gameserverId: [gameServerId] } },
+    { enabled: itemName !== '' }
+  );
+  const items = data?.pages.flatMap((page) => page.data) ?? [];
+
+  const renderIcon = (gameServer: GameServerOutputDTO, item: ItemsOutputDTO) => {
+    if (item.code && gameServer && gameServerTypeToIconFolderMap[gameServer.type] !== 'Mock') {
+      return (
+        <Avatar
+          size="tiny"
+          src={`/icons/${gameServerTypeToIconFolderMap[gameServer.type]}/${item.code}.png`}
+          alt={`Item icon of ${item.name}`}
+        />
+      );
+    }
+
+    return (
+      <Avatar size="tiny" alt={item.name}>
+        {getInitials('unknown')}
+      </Avatar>
+    );
+  };
+
+  if (!gameServer) {
+    return <div>unable to show items</div>;
+  }
+
+  return (
+    <SelectQueryField
+      name={name}
+      hint={hint}
+      label={label}
+      size={size}
+      loading={loading}
+      disabled={disabled}
+      inPortal={inPortal}
+      readOnly={readOnly}
+      required={required}
+      hasMargin={hasMargin}
+      description={description}
+      placeholder={placeholder}
+      handleInputValueChange={(value) => setItemName(value)}
+      isLoadingData={isLoadingGameServer || isLoadingItems}
+      control={control}
+    >
+      <SelectQueryField.OptionGroup label="options">
+        {items.map((item) => (
+          <SelectQueryField.Option value={item.id} label={item.name}>
+            <Inner>
+              {renderIcon(gameServer, item)}
+              <span>{item.name}</span>
+            </Inner>
+          </SelectQueryField.Option>
+        ))}
+      </SelectQueryField.OptionGroup>
+    </SelectQueryField>
+  );
+};
--- packages/web-main/src/components/selects/index.tsx
+++ packages/web-main/src/components/selects/index.tsx
@@ -1,7 +1,11 @@
-import { SelectFieldProps } from '@takaro/lib-components';
+import { SelectFieldProps, SelectQueryFieldProps } from '@takaro/lib-components';
 
 // TODO: implement multiSelect
-export type CustomSelectProps = Omit<SelectFieldProps, 'render' | 'children' | 'enableFilter' | 'multiSelect'>;
+export type CustomSelectProps = Omit<SelectFieldProps, 'render' | 'children' | 'enableFilter'>;
+export type CustomQuerySelectProps = Omit<
+  SelectQueryFieldProps,
+  'children' | 'handleInputValueChange' | 'isLoadingData' | 'debounce'
+>;
 
 export { GameServerSelect } from './GameServerSelect';
 export { RolesSelect } from './RolesSelect';
