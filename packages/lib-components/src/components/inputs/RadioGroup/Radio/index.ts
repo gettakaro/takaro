89a8be5a58c9ec9c346d4f54203413fd862c9bea
Emiel Van Severen <35073890+emielvanseveren@users.noreply.github.com>
2024-01-28 17:22:45 +0100

    Refactor of RadioGroup (#913)
    Co-authored-by: Niek Candaele <22315101+niekcandaele@users.noreply.github.com>

--- packages/lib-components/src/components/inputs/RadioGroup/Radio/index.ts
+++
@@ -1,5 +0,0 @@
-export { GenericRadio } from './Generic';
-export type { RadioProps } from './Generic';
-
-// Type that should only be available on controlled and uncontrolled.
-export type LabelPosition = { labelPosition: 'left' | 'right' };
