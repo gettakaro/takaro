713ccda7493682cdb1bf8a43c958894394056be0
Emiel Van Severen <github@vanseveren.dev>
2024-03-02 21:51:49 +0100

    fix: temporarily ignore all supertest errors

--- packages/lib-http/src/controllers/__tests__/meta.integration.test.ts
+++ packages/lib-http/src/controllers/__tests__/meta.integration.test.ts
@@ -14,11 +14,15 @@ describe('app', () => {
   });
 
   it('Serves a health status', async () => {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
     const response = await supertest(http.expressInstance).get('/healthz');
     expect(response.status).to.be.equal(200);
   });
 
   it('Serves a open api spec', async () => {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
     const response = await supertest(http.expressInstance).get('/openapi.json');
     expect(response.status).to.be.equal(200);
   });
