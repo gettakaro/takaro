e4c5562e4416ec6bf83cd290add6176cbe803e0f
Emiel Van Severen <github@vanseveren.dev>
2024-03-05 11:47:41 +0100

    Fix: delete module test

--- packages/e2e/src/web-main/pages/ModuleDefinitionsPage.ts
+++ packages/e2e/src/web-main/pages/ModuleDefinitionsPage.ts
@@ -83,7 +83,7 @@ export class ModuleDefinitionsPage extends BasePage {
     await this.page.getByRole('menuitem', { name: 'Delete module' }).click();
     await this.page.getByPlaceholder(name).fill(name);
     await this.page.getByRole('button', { name: 'Delete module' }).click();
-    await expect(this.page.getByText(name)).not.toBeVisible();
+    await expect(this.page.getByText(name)).toHaveCount(0);
   }
 
   // ===============================
