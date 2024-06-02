dee8c5192d417790d487e1da3d0ce9b8013da7d2
Emiel Van Severen <github@vanseveren.dev>
2024-06-02 18:56:07 +0200

    Chore: add wrapper for account pages that centers the page

---
+++ packages/web-main/src/routes/_single-page.tsx
@@ -0,0 +1,24 @@
+import { createFileRoute } from '@tanstack/react-router';
+import { Outlet } from '@tanstack/react-router';
+import { styled } from '@takaro/lib-components';
+
+export const Route = createFileRoute('/_single-page')({
+  component: LayoutComponent,
+});
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: calc(100vh - 200px);
+  margin-top: 200px;
+`;
+
+function LayoutComponent() {
+  return (
+    <Container>
+      <Outlet />
+    </Container>
+  );
+}
