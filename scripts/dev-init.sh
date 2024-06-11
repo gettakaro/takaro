24701771944020d60c1cfceb00ea08d4551874c0
Niek Candaele <niek@candaele.dev>
2024-06-11 18:48:26 +0200

    docs: improve self hosting docs

--- scripts/dev-init.sh
+++ scripts/dev-init.sh
@@ -16,53 +16,10 @@ if test -e ".env.example" && ! test -e ".env"; then
 fi
 
 printHeader "Initializing datastores"
-
-mkdir -p _data
-ls -la _data
+./scripts/setup-data-folders.sh
 
 printHeader "Setup playwright reports directory"
 mkdir -p reports
-
-# The prometheus container runs as a user with UID and GID 65534
-# so we need to make sure the data directory is writable by that user
-if [ ! -d "./_data/prometheus" ]; then
-	mkdir -p ./_data/prometheus
-	if command -v sudo >/dev/null 2>&1; then
-		sudo chown -R 65534:65534 ./_data/prometheus
-	fi
-fi
-
-# Same for Grafana
-if [ ! -d "./_data/grafana" ]; then
-	mkdir -p ./_data/grafana
-	if command -v sudo >/dev/null 2>&1; then
-		sudo chown -R 472:472 ./_data/grafana
-	fi
-fi
-
-# Same for Postgres
-if [ ! -d "./_data/db" ]; then
-	mkdir -p ./_data/db
-	if command -v sudo >/dev/null 2>&1; then
-		sudo chown -R 999:0 ./_data/db
-	fi
-fi
-
-# Same for Hydra 
-if [ ! -d "./_data/hydra-db" ]; then
-	mkdir -p ./_data/hydra-db
-	if command -v sudo >/dev/null 2>&1; then
-		sudo chown -R 999:0 ./_data/hydra-db
-	fi
-fi
-
-# Same for Kratos 
-if [ ! -d "./_data/kratos-db" ]; then
-	mkdir -p ./_data/kratos-db
-	if command -v sudo >/dev/null 2>&1; then
-		sudo chown -R 999:0 ./_data/kratos-db
-	fi
-fi
 
 printHeader "Installing node dependencies"
 npm ci
