12b34a2f91264413ff4a237072f2c5a1f1cc7f55
Emiel Van Severen <github@vanseveren.dev>
2024-06-16 16:15:48 +0200

    Feat: add queries for shopListing crud

---
+++ packages/web-main/src/queries/shopListing.tsx
@@ -0,0 +1,90 @@
+import {
+  APIOutput,
+  ShopListingCreateDTO,
+  ShopListingOutputArrayDTOAPI,
+  ShopListingOutputDTO,
+  ShopListingSearchInputDTO,
+  ShopListingUpdateDTO,
+} from '@takaro/apiclient';
+import { infiniteQueryOptions, queryOptions, useMutation, useQueryClient } from '@tanstack/react-query';
+import { AxiosError } from 'axios';
+import { getApiClient } from 'util/getApiClient';
+import { hasNextPage, mutationWrapper, queryParamsToArray } from './util';
+
+export const shopListingKeys = {
+  all: ['shopListing'] as const,
+  list: () => [...shopListingKeys.all, 'list'] as const,
+  detail: (shopListingId: string) => [...shopListingKeys.all, 'detail', shopListingId] as const,
+};
+
+export const shopListingQueryOptions = (shopListingId: string) =>
+  queryOptions<ShopListingOutputDTO, AxiosError<ShopListingOutputDTO>>({
+    queryKey: shopListingKeys.detail(shopListingId),
+    queryFn: async () => (await getApiClient().shop.shopControllerGetOne(shopListingId)).data.data,
+  });
+
+export const shopListingInfiniteQueryOptions = (queryParams: ShopListingSearchInputDTO = {}) => {
+  return infiniteQueryOptions<ShopListingOutputArrayDTOAPI, AxiosError<ShopListingOutputArrayDTOAPI>>({
+    queryKey: [...shopListingKeys.list(), 'infinite', ...queryParamsToArray(queryParams)],
+    queryFn: async () => (await getApiClient().shop.shopControllerSearch(queryParams)).data,
+    initialPageParam: 0,
+    getNextPageParam: (lastPage) => hasNextPage(lastPage.meta),
+  });
+};
+
+export const useShopListingCreate = () => {
+  const queryClient = useQueryClient();
+
+  return mutationWrapper<ShopListingOutputDTO, ShopListingCreateDTO>(
+    useMutation<ShopListingOutputDTO, AxiosError<ShopListingOutputDTO>, ShopListingCreateDTO>({
+      mutationFn: async (shopListing) => (await getApiClient().shop.shopControllerCreate(shopListing)).data.data,
+      onSuccess: (newShopListing) => {
+        queryClient.invalidateQueries({ queryKey: shopListingKeys.list() });
+        queryClient.setQueryData(shopListingKeys.detail(newShopListing.id), newShopListing);
+      },
+    }),
+    {}
+  );
+};
+
+interface ShopListingDelete {
+  id: string;
+}
+
+export const useShopListingRemove = () => {
+  return mutationWrapper<APIOutput, ShopListingOutputDTO>(
+    useMutation<APIOutput, AxiosError<APIOutput>, ShopListingDelete>({
+      mutationFn: async ({ id }) => (await getApiClient().shop.shopControllerDelete(id)).data,
+      onSuccess: async () => {
+        // await queryClient.invalidateQueries({ queryKey: shopListingKeys.list() });
+        //await queryClient.invalidateQueries({
+        //  queryKey: shopListingKeys.detail(removedShopListing.id),
+        //});
+      },
+    }),
+    {}
+  );
+};
+
+interface ShopListingUpdate {
+  shopListingId: string;
+  shopListingDetails: ShopListingUpdateDTO;
+}
+
+export const useShopListingUpdate = () => {
+  const apiClient = getApiClient();
+  const queryClient = useQueryClient();
+
+  return mutationWrapper<ShopListingOutputDTO, ShopListingUpdate>(
+    useMutation<ShopListingOutputDTO, AxiosError<ShopListingOutputDTO>, ShopListingUpdate>({
+      mutationFn: async ({ shopListingId, shopListingDetails }: ShopListingUpdate) => {
+        return (await apiClient.shop.shopControllerUpdate(shopListingId, shopListingDetails)).data.data;
+      },
+      onSuccess: async (updatedShopListing) => {
+        await queryClient.invalidateQueries({ queryKey: shopListingKeys.list() });
+        queryClient.setQueryData(shopListingKeys.detail(updatedShopListing.id), updatedShopListing);
+      },
+    }),
+    {}
+  );
+};
