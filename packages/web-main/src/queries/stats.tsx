d6efc26d1565ff287252ba2b27305de1b703a695
Emiel Van Severen <github@vanseveren.dev>
2024-06-16 16:56:35 +0200

    Feat: add query for activity stats

--- packages/web-main/src/queries/stats.tsx
+++ packages/web-main/src/queries/stats.tsx
@@ -1,6 +1,10 @@
 import { queryOptions } from '@tanstack/react-query';
 import { getApiClient } from 'util/getApiClient';
-import { StatsOutputDTO } from '@takaro/apiclient';
+import {
+  StatsControllerGetActivityStatsDataTypeEnum,
+  StatsControllerGetActivityStatsTimeTypeEnum,
+  StatsOutputDTO,
+} from '@takaro/apiclient';
 import { AxiosError } from 'axios';
 
 type StatsOutput = { values: Array<[number, number]> };
@@ -9,6 +13,7 @@ export const statsKeys = {
   ping: (playerId: string, gameServerId: string) => ['ping', playerId, gameServerId] as const,
   currency: (playerId: string, gameServerId: string) => ['currency', playerId, gameServerId] as const,
   playersOnline: (gameServerId?: string) => ['players-online', gameServerId] as const,
+  activity: (gameServerId?: string) => ['activity', gameServerId] as const,
 };
 
 export const PingStatsQueryOptions = (playerId: string, gameServerId: string) => {
@@ -29,5 +34,17 @@ export const PlayersOnlineStatsQueryOptions = (gameServerId?: string) => {
   return queryOptions<StatsOutputDTO, AxiosError<StatsOutputDTO>, StatsOutput>({
     queryKey: statsKeys.playersOnline(gameServerId),
     queryFn: async () => (await getApiClient().stats.statsControllerGetPlayerOnlineStats(gameServerId)).data.data,
+  });
+};
+
+export const ActivityStatsQueryOptions = (
+  gameServerId: string,
+  timeType: StatsControllerGetActivityStatsTimeTypeEnum,
+  dateType: StatsControllerGetActivityStatsDataTypeEnum
+) => {
+  return queryOptions<StatsOutputDTO, AxiosError<StatsOutputDTO>, StatsOutput>({
+    queryKey: statsKeys.activity(gameServerId),
+    queryFn: async () =>
+      (await getApiClient().stats.statsControllerGetActivityStats(timeType, dateType, gameServerId)).data.data,
   });
 };
