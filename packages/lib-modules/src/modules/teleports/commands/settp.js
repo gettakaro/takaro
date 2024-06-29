16673663ed2d26ec56d3d56fef940bcb447a2c77
Niek Candaele <22315101+niekcandaele@users.noreply.github.com>
2024-06-29 14:15:25 +0200

    Update settp.js (#1056)

--- packages/lib-modules/src/modules/teleports/commands/settp.js
+++ packages/lib-modules/src/modules/teleports/commands/settp.js
@@ -20,7 +20,7 @@ async function main() {
 
   const allPlayerTeleports = await takaro.variable.variableControllerSearch({
     search: {
-      key: getVariableKey('tp_'),
+      key: getVariableKey(''),
     },
     filters: {
       gameServerId: [gameServerId],
