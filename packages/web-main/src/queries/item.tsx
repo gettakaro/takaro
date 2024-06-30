bb3308380f0b9e546c7dd98bab9eb4c2d68badd7
Emiel Van Severen <github@vanseveren.dev>
2024-06-30 11:32:32 +0200

    Chore: impl changes in item api

--- packages/web-main/src/queries/item.tsx
+++ packages/web-main/src/queries/item.tsx
@@ -1,16 +1,8 @@
-import {
-  IdUuidDTO,
-  IdUuidDTOAPI,
-  ItemCreateDTO,
-  ItemOutputArrayDTOAPI,
-  ItemSearchInputDTO,
-  ItemsOutputDTO,
-  ItemUpdateDTO,
-} from '@takaro/apiclient';
-import { useMutation, useQueryClient, queryOptions, infiniteQueryOptions } from '@tanstack/react-query';
+import { ItemOutputArrayDTOAPI, ItemSearchInputDTO, ItemsOutputDTO } from '@takaro/apiclient';
+import { queryOptions, infiniteQueryOptions } from '@tanstack/react-query';
 import { AxiosError } from 'axios';
 import { getApiClient } from 'util/getApiClient';
-import { hasNextPage, mutationWrapper, queryParamsToArray } from './util';
+import { hasNextPage, queryParamsToArray } from './util';
 
 export const itemKeys = {
   all: ['items'] as const,
@@ -38,65 +30,3 @@ export const itemQueryOptions = (itemId: string) =>
     queryKey: itemKeys.detail(itemId),
     queryFn: async () => (await getApiClient().item.itemControllerFindOne(itemId)).data.data,
   });
-
-export const useItemCreate = () => {
-  const apiClient = getApiClient();
-  const queryClient = useQueryClient();
-
-  return mutationWrapper<ItemsOutputDTO, ItemCreateDTO>(
-    useMutation<ItemsOutputDTO, AxiosError<ItemsOutputDTO>, ItemCreateDTO>({
-      mutationFn: async (item) => (await apiClient.item.itemControllerCreate(item)).data.data,
-      onSuccess: (newItem) => {
-        queryClient.invalidateQueries({ queryKey: itemKeys.list() });
-        queryClient.setQueryData(itemKeys.detail(newItem.id), newItem);
-      },
-    }),
-    {}
-  );
-};
-
-interface ItemDelete {
-  id: string;
-}
-
-export const useItemRemove = () => {
-  const apiClient = getApiClient();
-  const queryClient = useQueryClient();
-
-  return mutationWrapper<IdUuidDTO, ItemDelete>(
-    useMutation<IdUuidDTO, AxiosError<IdUuidDTOAPI>, ItemDelete>({
-      mutationFn: async ({ id }) => (await apiClient.item.itemControllerDelete(id)).data.data,
-      onSuccess: async (removedItem: IdUuidDTO) => {
-        await queryClient.invalidateQueries({ queryKey: itemKeys.list() });
-
-        await queryClient.invalidateQueries({
-          queryKey: itemKeys.detail(removedItem.id),
-        });
-      },
-    }),
-    {}
-  );
-};
-
-interface ItemUpdate {
-  itemId: string;
-  itemDetails: ItemUpdateDTO;
-}
-
-export const useItemUpdate = () => {
-  const apiClient = getApiClient();
-  const queryClient = useQueryClient();
-
-  return mutationWrapper<ItemsOutputDTO, ItemUpdate>(
-    useMutation<ItemsOutputDTO, AxiosError<ItemsOutputDTO>, ItemUpdate>({
-      mutationFn: async ({ itemId, itemDetails }: ItemUpdate) => {
-        return (await apiClient.item.itemControllerUpdate(itemId, itemDetails)).data.data;
-      },
-      onSuccess: async (updatedItem) => {
-        await queryClient.invalidateQueries({ queryKey: itemKeys.list() });
-        queryClient.setQueryData(itemKeys.detail(updatedItem.id), updatedItem);
-      },
-    }),
-    {}
-  );
-};
