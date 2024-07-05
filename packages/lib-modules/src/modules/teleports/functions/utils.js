39519f33db85557dc7d76014ddca097909e02504
Niek Candaele <niek@candaele.dev>
2024-07-05 16:13:04 +0200

    fix: for teleport list when a server has many teleports (more than default pagination limit)

--- packages/lib-modules/src/modules/teleports/functions/utils.js
+++ packages/lib-modules/src/modules/teleports/functions/utils.js
@@ -1,11 +1,27 @@
 import { takaro, data } from '@takaro/helpers';
 
-export function getVariableKey(tpName) {
-  return `tp_${tpName}`;
+export function getVariableKey(tpName, pub = false) {
+  if (pub && tpName) return `pubtp_${tpName}`;
+  if (pub && !tpName) return 'pubtp_';
+  if (tpName) return `tp_${tpName}`;
+  return 'tp_';
 }
 
-export async function findTp(tpName, playerId) {
+export async function findTp(tpName, playerId, pub = false) {
   const { gameServerId, module: mod } = data;
+
+  if (pub) {
+    return takaro.variable.variableControllerSearch({
+      filters: {
+        key: [getVariableKey(tpName, true)],
+        gameServerId: [gameServerId],
+        playerId: [playerId].filter(Boolean),
+        moduleId: [mod.moduleId],
+      },
+      sortBy: 'key',
+      sortDirection: 'asc',
+    });
+  }
 
   return takaro.variable.variableControllerSearch({
     filters: {
