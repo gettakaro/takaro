0f507e35b90b7025e7b3144abd66d9df6bf24fb0
Emiel Van Severen <github@vanseveren.dev>
2024-07-13 23:55:49 +0200

    Chore: add type completion for 2 new env vars

--- packages/web-main/src/vite-env.d.ts
+++ packages/web-main/src/vite-env.d.ts
@@ -3,6 +3,8 @@
 interface ImportMetaEnv {
   readonly VITE_API: string;
   readonly VITE_ORY_URL: string;
+  readonly VITE_POSTHOG_API_URL: string;
+  readonly VITE_POSTHOG_PUBLIC_API_KEY: string;
 }
 
 interface ImportMeta {
