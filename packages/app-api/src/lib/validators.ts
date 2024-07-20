4b61bd0b1b449b2ab5faa819743ed979dc99a679
Emiel Van Severen <github@vanseveren.dev>
2024-07-20 16:12:48 +0200

    Chore: remove unused iduuid types

--- packages/app-api/src/lib/validators.ts
+++ packages/app-api/src/lib/validators.ts
@@ -1,15 +1,11 @@
 import {
   IsUUID,
-  ValidateNested,
   ValidationOptions,
   ValidatorConstraint,
   ValidatorConstraintInterface,
   isUUID,
   registerDecorator,
 } from 'class-validator';
-import { TakaroDTO } from '@takaro/util';
-import { Type } from 'class-transformer';
-import { APIOutput } from '@takaro/http';
 
 export class ParamId {
   @IsUUID('4')
@@ -27,17 +23,6 @@ export class PogParam {
 
   @IsUUID('4')
   playerId!: string;
-}
-
-export class IdUuidDTO extends TakaroDTO<IdUuidDTO> {
-  @IsUUID('4')
-  id!: string;
-}
-
-export class IdUuidDTOAPI extends APIOutput<IdUuidDTO> {
-  @Type(() => IdUuidDTO)
-  @ValidateNested()
-  declare data: IdUuidDTO;
 }
 
 @ValidatorConstraint({ async: true })
