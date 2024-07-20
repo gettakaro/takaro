c7270d17209871799112106dc3d171d461a6e2bc
Niek Candaele <22315101+niekcandaele@users.noreply.github.com>
2024-07-20 20:28:03 +0200

    fix: ci (#1077)
    Co-authored-by: Emiel Van Severen <github@vanseveren.dev>

--- scripts/integration-tests.mjs
+++ scripts/integration-tests.mjs
@@ -13,7 +13,7 @@ async function waitUntilHealthyHttp(url, maxRetries = 5) {
     if (stdout === '200') {
       return;
     }
-  } catch (err) { }
+  } catch (err) {}
 
   if (maxRetries > 0) {
     await sleep(1000);
