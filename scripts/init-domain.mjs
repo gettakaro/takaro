2113c0dc8d5a02d32279249710a7ed688f444902
Niek Candaele <niek@candaele.dev>
2024-08-22 12:51:59 +0200

    fix: fixes to init domain script

--- scripts/init-domain.mjs
+++ scripts/init-domain.mjs
@@ -1,7 +1,6 @@
 #!/bin/node
 
 import { Client, AdminClient } from '@takaro/apiclient';
-import crypto from 'crypto';
 
 const takaroHost = process.env.TAKARO_HOST;
 const adminClientSecret = process.env.ADMIN_CLIENT_SECRET;
@@ -9,13 +8,9 @@ const adminClientSecret = process.env.ADMIN_CLIENT_SECRET;
 if (!takaroHost) throw new Error('TAKARO_HOST is not set');
 if (!adminClientSecret) throw new Error('ADMIN_CLIENT_SECRET is not set');
 
-const userEmail = process.env.TAKARO_USER_EMAIL;
 const domainName = process.env.TAKARO_DOMAIN_NAME;
 
-if (!userEmail) throw new Error('TAKARO_USER_EMAIL is not set');
 if (!domainName) throw new Error('TAKARO_DOMAIN_NAME is not set');
-
-const userPassword = crypto.randomBytes(25).toString('base64');
 
 const adminClient = new AdminClient({
   url: takaroHost,
@@ -38,27 +33,6 @@ async function main() {
 
   console.log(`Created a domain with id ${domainRes.data.data.createdDomain.id}`);
   console.log(`Root user: ${domainRes.data.data.rootUser.email} / ${domainRes.data.data.password}`);
-
-  const client = new Client({
-    url: takaroHost,
-    auth: {
-      username: domainRes.data.data.rootUser.email,
-      password: domainRes.data.data.password,
-    },
-    log: false,
-  });
-
-  await client.login();
-
-  const userRes = await client.user.userControllerCreate({
-    email: userEmail,
-    password: userPassword,
-    name: userEmail.split('@')[0],
-  });
-
-  console.log(`Created a user: ${userRes.data.data.email} / ${userPassword}`);
-
-  await client.user.userControllerAssignRole(userRes.data.data.id, domainRes.data.data.rootRole.id);
 }
 
 main().catch(console.error);
