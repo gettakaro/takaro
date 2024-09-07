0422f04225db80b34470f385deace355bc545045
Niek Candaele <niek@candaele.dev>
2024-09-07 08:59:32 +0200

    feat: allow configuring admin auth in api docs

--- packages/lib-http/src/controllers/meta.ts
+++ packages/lib-http/src/controllers/meta.ts
@@ -66,9 +66,9 @@ export class Meta {
           securitySchemes: {
             adminAuth: {
               description: 'Used for system administration, like creating or deleting domains',
-              type: 'http',
-              scheme: 'bearer',
-              bearerFormat: 'JWT',
+              type: 'apiKey',
+              in: 'header',
+              name: 'x-takaro-admin-token',
             },
             domainAuth: {
               description: 'Used for anything inside a domain. Players, GameServers, etc.',
@@ -153,7 +153,7 @@ export class Meta {
 
           show-method-in-nav-bar="as-colored-block"
           show-header="false"
-          allow-authentication="false"
+          allow-authentication="true"
           allow-server-selection="false"
 
           schema-style="table"
