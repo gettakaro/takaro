b74f2e8ee6366c0345ce85d18e1db5253b1ad32f
Emiel Van Severen <github@vanseveren.dev>
2024-09-07 15:16:28 +0200

    Feat: redirect players without permissions to shop by default

--- packages/web-main/src/routes/_auth/_global.tsx
+++ packages/web-main/src/routes/_auth/_global.tsx
@@ -1,7 +1,22 @@
-import { createFileRoute } from '@tanstack/react-router';
+import { createFileRoute, redirect } from '@tanstack/react-router';
 import { BaseLayout } from 'components/BaseLayout';
+import { hasPermission } from 'hooks/useHasPermission';
+import { gameServersQueryOptions } from 'queries/gameserver';
+import { userMeQueryOptions } from 'queries/user';
 
 export const Route = createFileRoute('/_auth/_global')({
+  beforeLoad: async ({ context }) => {
+    // Redirect users to the first available gameservershop if they don't have MANAGE_GAMESERVERS permission.
+    // Later this should be smarter and redirect to specific pages depending on the available permissions.
+    const session = await context.queryClient.ensureQueryData(userMeQueryOptions());
+    const canManageGameServers = hasPermission(session, ['MANAGE_GAMESERVERS']);
+    if (!canManageGameServers) {
+      const gameservers = await context.queryClient.ensureQueryData(gameServersQueryOptions());
+      if (gameservers.length > 0) {
+        throw redirect({ to: '/gameserver/$gameServerId/shop', params: { gameServerId: gameservers[0].id } });
+      }
+    }
+  },
   component: Component,
 });
 
