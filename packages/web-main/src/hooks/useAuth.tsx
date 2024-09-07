fad8f1f08329dd3b627f85fa3e50063f7e7d183f
Emiel Van Severen <github@vanseveren.dev>
2024-09-07 13:11:31 +0200

    Chore: get rid of localstorage session

--- packages/web-main/src/hooks/useAuth.tsx
+++ packages/web-main/src/hooks/useAuth.tsx
@@ -3,46 +3,13 @@ import { MeOutputDTO } from '@takaro/apiclient';
 import { createContext, useCallback, useContext } from 'react';
 import { useOry } from './useOry';
 import * as Sentry from '@sentry/react';
-import { DateTime } from 'luxon';
 import { getApiClient } from 'util/getApiClient';
 import { usePostHog } from 'posthog-js/react';
-
-const SESSION_EXPIRES_AFTER_MINUTES = 5;
-
-interface ExpirableSession {
-  session: MeOutputDTO;
-  expiresAt: string;
-}
 
 export interface IAuthContext {
   logOut: () => Promise<void>;
   getSession: () => Promise<MeOutputDTO>;
   login: (user: MeOutputDTO) => void;
-}
-
-function getLocalSession() {
-  const expirableSessionString = localStorage.getItem('session');
-  if (!expirableSessionString) {
-    return null;
-  }
-
-  const expirableSession: ExpirableSession = JSON.parse(expirableSessionString);
-  if (DateTime.fromISO(expirableSession.expiresAt) < DateTime.now()) {
-    return null;
-  }
-  return expirableSession.session;
-}
-
-function setLocalSession(session: MeOutputDTO | null) {
-  if (session) {
-    const expirableSession: ExpirableSession = {
-      session,
-      expiresAt: DateTime.now().plus({ minutes: SESSION_EXPIRES_AFTER_MINUTES }).toISO(),
-    };
-    localStorage.setItem('session', JSON.stringify(expirableSession));
-  } else {
-    localStorage.removeItem('session');
-  }
 }
 
 export const AuthContext = createContext<IAuthContext | null>(null);
@@ -54,7 +21,6 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
 
   const logOut = useCallback(async () => {
     const logoutFlowRes = await oryClient.createBrowserLogoutFlow();
-    setLocalSession(null);
     queryClient.clear();
     window.location.href = logoutFlowRes.data.logout_url;
     // Extra clean up is done in /logout-return
@@ -62,7 +28,6 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
   }, [oryClient, queryClient]);
 
   const login = useCallback((session: MeOutputDTO) => {
-    setLocalSession(session);
     Sentry.setUser({ id: session.user.id, email: session.user.email, username: session.user.name });
     posthog.identify(session.user.idpId, {
       email: session.user.email,
@@ -71,12 +36,6 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
   }, []);
 
   const getSession = async function (): Promise<MeOutputDTO> {
-    const session = getLocalSession();
-
-    if (session) {
-      return session;
-    }
-
     try {
       const newSession = (
         await getApiClient().user.userControllerMe({
@@ -85,12 +44,10 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
           },
         })
       ).data.data;
-      setLocalSession(newSession);
       return newSession;
     } catch (_error) {
       // logout if session is invalid
       localStorage.removeItem('session');
-      setLocalSession(null);
       queryClient.clear();
       window.location.href = '/login';
       throw 'should have no session and should be redirected to login';
