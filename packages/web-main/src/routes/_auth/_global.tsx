dddd32af19dcdc2675187dea61db27577aa0304d
Emiel Van Severen <github@vanseveren.dev>
2024-09-07 16:05:46 +0200

    Fix: only navigate when location pathname is /

--- packages/web-main/src/routes/_auth/_global.tsx
+++ packages/web-main/src/routes/_auth/_global.tsx
@@ -5,12 +5,13 @@ import { gameServersQueryOptions } from 'queries/gameserver';
 import { userMeQueryOptions } from 'queries/user';
 
 export const Route = createFileRoute('/_auth/_global')({
-  beforeLoad: async ({ context }) => {
+  beforeLoad: async ({ context, location }) => {
     // Redirect users to the first available gameservershop if they don't have MANAGE_GAMESERVERS permission.
+    // and they logged in to the default '/' path.
     // Later this should be smarter and redirect to specific pages depending on the available permissions.
     const session = await context.queryClient.ensureQueryData(userMeQueryOptions());
     const canManageGameServers = hasPermission(session, ['MANAGE_GAMESERVERS']);
-    if (!canManageGameServers) {
+    if (location.pathname == '/' && !canManageGameServers) {
       const gameservers = await context.queryClient.ensureQueryData(gameServersQueryOptions());
       if (gameservers.length > 0) {
         throw redirect({ to: '/gameserver/$gameServerId/shop', params: { gameServerId: gameservers[0].id } });
