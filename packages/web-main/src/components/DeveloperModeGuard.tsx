203aefc63b7cfba4c7c06d7f4b0cb2c4f18133e5
Emiel Van Severen <github@vanseveren.dev>
2024-09-22 10:32:54 +0200

    Chore: developer mode frontend

---
+++ packages/web-main/src/components/DeveloperModeGuard.tsx
@@ -0,0 +1,22 @@
+import { FC, PropsWithChildren } from 'react';
+import { globalGameServerSetingQueryOptions } from 'queries/setting';
+import { useQuery } from '@tanstack/react-query';
+
+interface DeveloperModeGuardProps {
+  /// When used in loop or iterator, you sometimes want to guard for some of the items but not for all
+  enabled?: boolean;
+}
+
+export const DeveloperModeGuard: FC<PropsWithChildren<DeveloperModeGuardProps>> = ({ children, enabled = true }) => {
+  const { data, isLoading } = useQuery({ ...globalGameServerSetingQueryOptions('developerMode'), enabled });
+
+  if (!enabled) {
+    return <>{children}</>;
+  }
+
+  if (isLoading || !data || data.value === 'false') {
+    return null;
+  }
+
+  return <>{children}</>;
+};
