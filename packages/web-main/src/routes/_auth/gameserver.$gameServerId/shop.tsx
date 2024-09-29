5d90ebe3dab694479cfc3a09353f286240f197b5
Emiel Van Severen <github@vanseveren.dev>
2024-09-29 21:45:56 +0200

    Chore: show claimable order (only up to 100) as badge

--- packages/web-main/src/routes/_auth/gameserver.$gameServerId/shop.tsx
+++ packages/web-main/src/routes/_auth/gameserver.$gameServerId/shop.tsx
@@ -1,10 +1,17 @@
 import { ErrorBoundary } from '@sentry/react';
-import { HorizontalNav, styled } from '@takaro/lib-components';
+import { ShopOrderUpdateDTOStatusEnum } from '@takaro/apiclient';
+import { Badge, HorizontalNav, styled, Tooltip } from '@takaro/lib-components';
+import { useQuery } from '@tanstack/react-query';
 import { Link, Outlet, createFileRoute } from '@tanstack/react-router';
 import { useDocumentTitle } from 'hooks/useDocumentTitle';
+import { shopOrdersQueryOptions } from 'queries/shopOrder';
 
+const shopOrdersQuery = shopOrdersQueryOptions({ filters: { status: [ShopOrderUpdateDTOStatusEnum.Paid] } });
 export const Route = createFileRoute('/_auth/gameserver/$gameServerId/shop')({
   component: Component,
+  loader: async ({ context }) => {
+    return await context.queryClient.ensureQueryData(shopOrdersQuery);
+  },
   pendingComponent: () => <div>Loading...</div>,
 });
 
@@ -17,7 +24,16 @@ const Container = styled.div`
 
 function Component() {
   const { gameServerId } = Route.useParams();
+  const loaderData = Route.useLoaderData();
+
+  const { data: shopOrders } = useQuery({
+    ...shopOrdersQuery,
+    initialData: loaderData,
+  });
+
   useDocumentTitle('Shop');
+
+  const unClaimedShopOrdersCount = shopOrders?.data.length ?? 0;
 
   return (
     <>
@@ -26,8 +42,25 @@ function Component() {
           <Link activeOptions={{ exact: true }} to="/gameserver/$gameServerId/shop" params={{ gameServerId }}>
             Shop
           </Link>
-          <Link activeOptions={{ exact: true }} to="/gameserver/$gameServerId/shop/orders" params={{ gameServerId }}>
-            Orders
+          <Link
+            activeOptions={{ exact: true }}
+            to="/gameserver/$gameServerId/shop/orders"
+            params={{ gameServerId }}
+            style={{ position: 'relative' }}
+          >
+            <span>
+              {unClaimedShopOrdersCount > 0 && (
+                <Tooltip>
+                  <Tooltip.Trigger asChild>
+                    <Badge variant="info" animate>
+                      {unClaimedShopOrdersCount}
+                    </Badge>
+                  </Tooltip.Trigger>
+                  <Tooltip.Content>You have {unClaimedShopOrdersCount} orders ready to be claimed!</Tooltip.Content>
+                </Tooltip>
+              )}
+              Orders
+            </span>
           </Link>
         </HorizontalNav>
       </Container>
