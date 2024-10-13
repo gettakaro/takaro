63f953f0ca6082c231b9f7434d7274778aed9236
Niek Candaele <niek@candaele.dev>
2024-10-13 18:22:15 +0200

    fix: when user has orders but no playerId linked anymore

--- packages/web-main/src/routes/_auth/gameserver.$gameServerId/-components/shop/shopOrderTableView.tsx
+++ packages/web-main/src/routes/_auth/gameserver.$gameServerId/-components/shop/shopOrderTableView.tsx
@@ -180,7 +180,11 @@ const ShopOrderActionsDataQueryWrapper: FC<{ shopOrder: ShopOrderOutputDTO }> = 
     return '';
   }
 
-  return <ShopOrderActions shopOrder={shopOrder} gameServerId={gameServerId} playerId={data.playerId!} />;
+  if (!data.playerId) {
+    return '';
+  }
+
+  return <ShopOrderActions shopOrder={shopOrder} gameServerId={gameServerId} playerId={data.playerId} />;
 };
 
 const ShopOrderActions: FC<ShopOrderActionsProps> = ({ shopOrder, gameServerId, playerId }) => {
@@ -257,5 +261,9 @@ export const UserToPlayer: FC<{ userId: string }> = ({ userId }) => {
     return 'unknown';
   }
 
-  return <PlayerContainer playerId={data.playerId!} />;
+  if (!data.playerId) {
+    return `user: ${data.name}`;
+  }
+
+  return <PlayerContainer playerId={data.playerId} />;
 };
