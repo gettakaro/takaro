de66a00e276ddb3061019e4c3ccda7797502c3f8
Niek Candaele <niek@candaele.dev>
2024-10-23 21:09:48 +0200

    fix: some module related stuff for new item config method

--- packages/lib-modules/src/modules/playerOnboarding/commands/starterkit.js
+++ packages/lib-modules/src/modules/playerOnboarding/commands/starterkit.js
@@ -7,7 +7,7 @@ async function main() {
 
   if (!items || items.length === 0) {
     throw new TakaroUserError(
-      'No starter kit items configured. Please ask your server administrator to configure this.'
+      'No starter kit items configured. Please ask your server administrator to configure this.',
     );
   }
 
@@ -25,13 +25,27 @@ async function main() {
 
   await data.player.pm('You are about to receive your starter kit...');
 
+  const itemRecords = (await takaro.item.itemControllerSearch({ filters: { id: items.map((_) => _.item) } })).data.data;
+  const fullItems = items.map((item) => {
+    const itemRecord = itemRecords.find((record) => record.id === item.item);
+    if (!itemRecord) {
+      throw new TakaroUserError(`Item with ID ${item.item} not found.`);
+    }
+    return {
+      code: itemRecord.code,
+      quality: item.quality,
+      amount: item.amount,
+    };
+  });
+
   await Promise.all(
-    items.map(async (item) => {
+    fullItems.map(async (item) => {
       return takaro.gameserver.gameServerControllerGiveItem(data.gameServerId, data.player.id, {
-        name: item,
-        amount: 1,
+        name: item.code,
+        quality: item.quality ?? '',
+        amount: item.amount,
       });
-    })
+    }),
   );
 
   await takaro.variable.variableControllerCreate({
