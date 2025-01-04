cd74ecc5ec5fd420e9df44179c727b3e2fa9719a
Emiel Van Severen <github@vanseveren.dev>
2025-01-04 15:28:47 +0100

    Fix(web-main): only show shop actions when correct permission

--- packages/web-main/src/routes/_auth/gameserver.$gameServerId/-components/shop/ShopView.tsx
+++ packages/web-main/src/routes/_auth/gameserver.$gameServerId/-components/shop/ShopView.tsx
@@ -17,6 +17,7 @@ import { useSnackbar } from 'notistack';
 import { useQuery } from '@tanstack/react-query';
 import { shopListingsQueryOptions } from 'queries/shopListing';
 import { TableListToggleButton, ViewType } from 'components/TableListToggleButton';
+import { PermissionsGuard } from 'components/PermissionsGuard';
 
 const Header = styled.div`
   display: flex;
@@ -108,40 +109,42 @@ export const ShopView: FC<ShopViewProps> = ({ gameServerId, currency, currencyNa
           )}
         </Header>
 
-        <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'row', gap: '10px' }}>
-          <Dropdown>
-            <Dropdown.Trigger asChild>
-              <Button icon={<MenuIcon />} text="Shop actions" />
-            </Dropdown.Trigger>
-            <Dropdown.Menu>
-              <DropdownMenu.Group divider>
-                <DropdownMenu.Item
-                  icon={<CreateNewShopListingIcon />}
-                  label="New shop listing"
-                  onClick={onClickCreateNewShopListing}
-                />
-                <DropdownMenu.Item
-                  icon={<ExportShopListingsToFileIcon />}
-                  label="Export listings to file"
-                  onClick={onClickExportShopListingsToFile}
-                />
-              </DropdownMenu.Group>
-              <DropdownMenu.Group label="Import methods" divider>
-                <DropdownMenu.Item
-                  icon={<ImportShopListingFromGameServerIcon />}
-                  label="Import listings from game server"
-                  onClick={onClickImportShopListingsFromGameServer}
-                />
-                <DropdownMenu.Item
-                  icon={<ImportShopListingsFromFileIcon />}
-                  label="Import listings from file"
-                  onClick={onClickImportShopListingsFromFile}
-                />
-              </DropdownMenu.Group>
-            </Dropdown.Menu>
-          </Dropdown>
-          <TableListToggleButton value={view} onChange={setView} />
-        </div>
+        <PermissionsGuard requiredPermissions={['MANAGE_SHOP_LISTINGS']}>
+          <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'row', gap: '10px' }}>
+            <Dropdown>
+              <Dropdown.Trigger asChild>
+                <Button icon={<MenuIcon />} text="Shop actions" />
+              </Dropdown.Trigger>
+              <Dropdown.Menu>
+                <DropdownMenu.Group divider>
+                  <DropdownMenu.Item
+                    icon={<CreateNewShopListingIcon />}
+                    label="New shop listing"
+                    onClick={onClickCreateNewShopListing}
+                  />
+                  <DropdownMenu.Item
+                    icon={<ExportShopListingsToFileIcon />}
+                    label="Export listings to file"
+                    onClick={onClickExportShopListingsToFile}
+                  />
+                </DropdownMenu.Group>
+                <DropdownMenu.Group label="Import methods" divider>
+                  <DropdownMenu.Item
+                    icon={<ImportShopListingFromGameServerIcon />}
+                    label="Import listings from game server"
+                    onClick={onClickImportShopListingsFromGameServer}
+                  />
+                  <DropdownMenu.Item
+                    icon={<ImportShopListingsFromFileIcon />}
+                    label="Import listings from file"
+                    onClick={onClickImportShopListingsFromFile}
+                  />
+                </DropdownMenu.Group>
+              </Dropdown.Menu>
+            </Dropdown>
+            <TableListToggleButton value={view} onChange={setView} />
+          </div>
+        </PermissionsGuard>
       </div>
 
       {view === 'table' && (
