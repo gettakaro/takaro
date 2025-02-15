8deb8b254cc632de03b0df3d0e5a501f41cfe950
Emiel Van Severen <github@vanseveren.dev>
2025-02-15 20:26:24 +0100

    chore(e2e): set it to null if configvar is not found

--- packages/web-main/src/util/getConfigVar.ts
+++ packages/web-main/src/util/getConfigVar.ts
@@ -30,7 +30,7 @@ export function getConfigVar(name: keyof typeof envMap) {
   if (metaEnvVar) {
     return metaEnvVar;
   }
-  throw new Error(`Environment variable ${envVarKey} is not defined`);
+  return null;
 }
 
 export function getTakaroVersionComponents(takaroVersionString: string) {
