41ba536033c201bcfcc69825e63a8db9117c2fe0
Niek Candaele <niek@candaele.dev>
2025-03-14 11:41:48 +0100

    feat: include reconciler in bull board

--- packages/lib-queues/src/bullboard.ts
+++ packages/lib-queues/src/bullboard.ts
@@ -2,15 +2,16 @@ import { createBullBoard } from '@bull-board/api';
 import { ExpressAdapter } from '@bull-board/express';
 import { BullMQAdapter } from '@bull-board/api/bullMQAdapter.js';
 import { queueService } from './QueueService.js';
+import { TakaroQueue } from './TakaroQueue.js';
 
 export function getBullBoard() {
   const serverAdapter = new ExpressAdapter();
   serverAdapter.setBasePath('/queues');
 
   const queues = Object.values(queueService.queues).map((queue) => new BullMQAdapter(queue.queue.bullQueue));
-
+  const reconcilerQueue = new TakaroQueue<Record<string, unknown>>('domainReconciler');
   createBullBoard({
-    queues,
+    queues: [...queues, new BullMQAdapter(reconcilerQueue.bullQueue)],
     serverAdapter: serverAdapter,
   });
 
