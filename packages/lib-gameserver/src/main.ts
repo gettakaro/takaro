8acba4a53d30397a8d9700b477f50e499de81d2a
Niek Candaele <niek@candaele.dev>
2025-04-21 09:45:06 +0200

    fix: build issue

--- packages/lib-gameserver/src/main.ts
+++ packages/lib-gameserver/src/main.ts
@@ -2,9 +2,6 @@ export * from './interfaces/GameServer.js';
 
 export { TakaroEmitter } from './TakaroEmitter.js';
 
-export { Mock } from './gameservers/mock/index.js';
-export { MockConnectionInfo, mockJsonSchema } from './gameservers/mock/connectionInfo.js';
-
 export { SevenDaysToDie } from './gameservers/7d2d/index.js';
 export { SdtdConnectionInfo, sdtdJsonSchema } from './gameservers/7d2d/connectionInfo.js';
 
