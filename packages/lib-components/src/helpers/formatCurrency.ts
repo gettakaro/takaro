fa1e8646f1ab726d3034903ebc79929c19ae24b2
Emiel Van Severen <github@vanseveren.dev>
2025-11-01 15:47:10 +0100

    feat(lib-components): add currency formatting helper

---
+++ packages/lib-components/src/helpers/formatCurrency.ts
@@ -0,0 +1,6 @@
+export function formatCurrency(value: number, decimals: number = 0): string {
+  return new Intl.NumberFormat('en-US', {
+    minimumFractionDigits: decimals,
+    maximumFractionDigits: decimals,
+  }).format(value);
+}
